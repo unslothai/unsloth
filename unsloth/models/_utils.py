2d5d88487463e76f75002be3b2704267ec96e68a
Daniel Han-Chen <danielhanchen@gmail.com>
2023-12-12 02:40:37 +1100

    tokenizer pad fix

--- unsloth/models/_utils.py
+++ unsloth/models/_utils.py
@@ -19,6 +19,14 @@ import warnings
 import gc
 warnings.filterwarnings(action = "ignore", category = UserWarning, module = "torch")
 import bitsandbytes as bnb
+from transformers.models.llama.modeling_llama import logger
+
+__version__ = "2023.12"
+__all__ = [
+    "prepare_model_for_kbit_training",
+    "patch_tokenizer",
+    "print_unsloth_message",
+]
 
 
 def prepare_model_for_kbit_training(
@@ -58,4 +66,39 @@ def prepare_model_for_kbit_training(
             model.get_input_embeddings().register_forward_hook(make_inputs_require_grad)
 
     return model
+pass
+
+
+def patch_tokenizer(model, tokenizer):
+    if not hasattr(tokenizer, "pad_token") or tokenizer.pad_token is None:
+        # Fixes https://github.com/unslothai/unsloth/issues/5
+        if hasattr(tokenizer, "unk_token"):
+            tokenizer.add_special_tokens({"pad_token" : tokenizer.unk_token})
+            tokenizer.pad_token = tokenizer.unk_token
+        else:
+            logger.warning_one(
+                f"{model.config._name_or_path} does not have a padding or unknown token!\n"\
+                f"Will use the EOS token of id {tokenizer.eos_token_id} as padding."
+            )
+            assert(hasattr(tokenizer, "eos_token"))
+            tokenizer.add_special_tokens({"pad_token" : tokenizer.eos_token})
+            tokenizer.pad_token = tokenizer.eos_token
+        config = model.config.update({"pad_token_id" : tokenizer.eos_token_id})
+    pass
+    return model, tokenizer
+pass
+
+
+def print_unsloth_message(name):
+    SUPPORTS_BFLOAT16 = torch.cuda.is_bf16_supported()
+    gpu_stats = torch.cuda.get_device_properties(0)
+    max_memory = round(gpu_stats.total_memory / 1024 / 1024 / 1024, 3)
+
+    statistics = \
+       f"==((====))==  Unsloth: Fast {name} patching release {__version__}\n"\
+       f"   \\\   /|    GPU: {gpu_stats.name}. Max memory: {max_memory} GB\n"\
+       f"O^O/ \_/ \\    CUDA compute capability = {gpu_stats.major}.{gpu_stats.minor}\n"\
+       f"\        /    Pytorch version: {torch.__version__}. CUDA Toolkit = {torch.version.cuda}\n"\
+       f' "-____-"     bfloat16 support = {str(SUPPORTS_BFLOAT16).upper()}\n'
+    print(statistics)
 pass
