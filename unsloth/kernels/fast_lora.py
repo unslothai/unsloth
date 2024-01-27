85e87d9ba2849c09ecc342ac1bce33a91a6638f1
Daniel Han-Chen <danielhanchen@gmail.com>
2024-01-27 19:03:04 +1100

    Swiglu

--- unsloth/kernels/fast_lora.py
+++ unsloth/kernels/fast_lora.py
@@ -229,14 +229,15 @@ class LoRA_MLP_New(torch.autograd.Function):
         dtype = X.dtype
 
         DW = matmul_lora(dY, downW.t(), downW_quant, downB, downA, downS)
-
-        e = e.float()
-        se = 1.0 / (1.0 + torch.exp(-e))
-        f = (se * e).to(dtype)
-        h = f * g
-        df = DW * f
-        dg = DW * g
-        de = (dg.float() * se * (1.0 + e * (1.0 - se))).to(dtype)
+        # e = e.float()
+        # se = 1.0 / (1.0 + torch.exp(-e))
+        # f = (se * e).to(dtype)
+        # h = f * g
+        # df = DW * f
+        # dg = DW * g
+        # de = (dg.float() * se * (1.0 + e * (1.0 - se))).to(dtype)
+        DW, e, g = swiglu_DWf_DW_dfg_kernel(DW, e, g)
+        h, df, de = DW, e, g
 
         # Down projection LoRA weights
         d_downA = h.t() @ (dY @ downB.t())
@@ -256,10 +257,8 @@ class LoRA_MLP_New(torch.autograd.Function):
         d_gateA *= gateS
         d_gateB *= gateS
 
-
         # dX  = matmul_lora(df, upW.t(), upW_quant, upB, upA, upS)
         # dX += matmul_lora(de, gateW.t(), gateW_quant, gateB, gateA, gateS)
-
         upW = fast_dequantize(upW.t(), upW_quant)
         dX = torch.matmul(df, upW.t(), out = X)
         del upW
