6a027a8292f83480fb2cdb7ee8e135733bd77b70
Daniel Han-Chen <danielhanchen@gmail.com>
2024-01-28 03:55:23 +1100

    Update llama.py

--- unsloth/models/llama.py
+++ unsloth/models/llama.py
@@ -635,6 +635,7 @@ def LlamaForCausalLM_fast_forward(
     logits = self.lm_head(hidden_states)
 
     loss = None
+    print(labels)
     if labels is not None:
         shift_logits = logits
         if not hasattr(self, "extra_ignored_labels"):
