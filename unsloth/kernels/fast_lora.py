363ffba1c241ebffa2a255162c13d681a352e7bc
Daniel Han-Chen <danielhanchen@gmail.com>
2024-01-27 20:17:08 +1100

    Update fast_lora.py

--- unsloth/kernels/fast_lora.py
+++ unsloth/kernels/fast_lora.py
@@ -283,7 +283,6 @@ pass
 
 from transformers.models.llama.modeling_llama import logger
 def apply_lora_mlp(self, X):
-    logger.warning_once("Hello!2")
     # gate = self.gate_proj(X)
     # up   = self.  up_proj(X)
     # h = torch.nn.functional.silu(gate) * up
