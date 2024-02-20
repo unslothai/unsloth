6aac6c4be8219f8fb0ba19d8a77b404ebbd9c2f6
Daniel Han-Chen <danielhanchen@gmail.com>
2024-02-20 19:50:29 +1100

    Update save.py

--- unsloth/save.py
+++ unsloth/save.py
@@ -852,24 +852,28 @@ def upload_to_huggingface(model, save_directory, token, method, extra = "", file
     pass
 
     from huggingface_hub import create_repo
-    create_repo(
-        repo_id   = save_directory,
-        token     = token,
-        repo_type = "model",
-        exist_ok  = True,
-    )
+    try:
+        create_repo(
+            repo_id   = save_directory,
+            token     = token,
+            repo_type = "model",
+            exist_ok  = False,
+            private   = private,
+        )
 
-    # Create model card
-    from huggingface_hub import ModelCard
-    content = MODEL_CARD.format(
-        username   = username,
-        base_model = model.config._name_or_path,
-        model_type = model.config.model_type,
-        method     = "",
-        extra      = extra,
-    )
-    card = ModelCard(content)
-    card.push_to_hub(save_directory, token = token)
+        # Create model card
+        from huggingface_hub import ModelCard
+        content = MODEL_CARD.format(
+            username   = username,
+            base_model = model.config._name_or_path,
+            model_type = model.config.model_type,
+            method     = "",
+            extra      = extra,
+        )
+        card = ModelCard(content)
+        card.push_to_hub(save_directory, token = token)
+    except:
+        pass
 
     if file_location is not None:
         # Now upload file
