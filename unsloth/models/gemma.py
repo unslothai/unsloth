61add47f37572d9f5eca0f464cede384b39dff7f
Daniel Han-Chen <danielhanchen@gmail.com>
2024-02-24 17:31:10 +1100

    Update gemma.py

--- unsloth/models/gemma.py
+++ unsloth/models/gemma.py
@@ -99,14 +99,16 @@ def GemmaAttention_fast_forward(
     if past_key_value is not None:
         kv_seq_len += past_key_value[0].shape[-2]
 
-    if False:#position_ids is None:
-        cos = self.rotary_emb.cos_cached
-        sin = self.rotary_emb.sin_cached
-        Q, K = fast_rope_embedding(Q, K, cos, sin)
-    else:
-        cos, sin = self.rotary_emb(V, seq_len = kv_seq_len)
-        Q, K = inplace_rope_embedding(Q, K, cos, sin, position_ids)
-    pass
+    # if False:#position_ids is None:
+    #     cos = self.rotary_emb.cos_cached
+    #     sin = self.rotary_emb.sin_cached
+    #     Q, K = fast_rope_embedding(Q, K, cos, sin)
+    # else:
+    #     cos, sin = self.rotary_emb(V, seq_len = kv_seq_len)
+    #     Q, K = inplace_rope_embedding(Q, K, cos, sin, position_ids)
+    # pass
+    cos, sin = self.rotary_emb(value_states, position_ids, seq_len=None)
+    query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, None)
 
     if past_key_value is not None:
         K = torch.cat([past_key_value[0], K], dim = 2)
@@ -586,8 +588,8 @@ class FastGemmaModel(FastLlamaModel):
         # Inferene can now be CUDAGraphed, but we shall retain the old rotary embeddings.
         # https://github.com/huggingface/transformers/pull/27931
         # https://github.com/huggingface/transformers/blob/v4.37.2/src/transformers/models/llama/modeling_llama.py
-        import transformers.models.gemma.modeling_gemma
-        transformers.models.gemma.modeling_gemma.GemmaRotaryEmbedding = LlamaRotaryEmbedding
+        # import transformers.models.gemma.modeling_gemma
+        # transformers.models.gemma.modeling_gemma.GemmaRotaryEmbedding = LlamaRotaryEmbedding
         return
     pass
 
