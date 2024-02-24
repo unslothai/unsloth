df8034d185255c2b6286a4fb44bcb8e8388ad302
Daniel Han-Chen <danielhanchen@gmail.com>
2024-02-25 04:48:34 +1100

    Update llama.py

--- unsloth/models/llama.py
+++ unsloth/models/llama.py
@@ -283,7 +283,7 @@ def LlamaAttention_fast_forward(
     if position_ids is None:
         cos = self.rotary_emb.cos_cached
         sin = self.rotary_emb.sin_cached
-        if cos in None:
+        if cos is None:
             # Need to call rotary_emb to init it
             cos, sin = self.rotary_emb(V, position_ids, seq_len = kv_seq_len)
         pass
