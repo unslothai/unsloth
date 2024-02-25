b55d7ad4b2aa97ad04a382fe0cf35b61ce7f537f
Daniel Han-Chen <danielhanchen@gmail.com>
2024-02-26 03:29:11 +1100

    Update cross_entropy_loss.py

--- unsloth/kernels/cross_entropy_loss.py
+++ unsloth/kernels/cross_entropy_loss.py
@@ -217,7 +217,7 @@ class Fast_CrossEntropyLoss(torch.autograd.Function):
         n_rows, n_cols = logits.shape
         grid = lambda meta: (n_rows, triton.cdiv(n_cols, meta["BLOCK_SIZE"]))
 
-        print(logits.stride(), dlosses.stride())
+        print(logits.stride(), dlosses.stride(), dlosses.shape)
         _cross_entropy_backward[grid](
             logits,   logits.stride(0),
             dlosses, dlosses.stride(0),
