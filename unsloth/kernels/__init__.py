65fbde6cd9a931d6510dff5a4d2d14ff0b1284cf
Daniel Han-Chen <danielhanchen@gmail.com>
2024-03-03 03:37:43 +1100

    Update __init__.py

--- unsloth/kernels/__init__.py
+++ unsloth/kernels/__init__.py
@@ -16,7 +16,12 @@ from .cross_entropy_loss import fast_cross_entropy_loss
 from .rms_layernorm import fast_rms_layernorm
 from .rope_embedding import fast_rope_embedding, inplace_rope_embedding
 from .swiglu import swiglu_fg_kernel, swiglu_DWf_DW_dfg_kernel
-from .geglu import geglu_forward_kernel, geglu_backward_kernel
+from .geglu import (
+	geglu_exact_forward_kernel,
+	geglu_exact_backward_kernel,
+	geglu_approx_forward_kernel,
+	geglu_approx_backward_kernel,
+)
 from .fast_lora import (
 	get_lora_parameters,
 	apply_lora_mlp_swiglu,
