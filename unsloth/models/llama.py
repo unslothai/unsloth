5e6c82aa6b1df95cdbd0351e3df74556a2849452
Daniel Han-Chen <danielhanchen@gmail.com>
2024-04-03 04:12:31 +1100

    Update llama.py

--- unsloth/models/llama.py
+++ unsloth/models/llama.py
@@ -224,12 +224,12 @@ torch_nn_functional_silu = torch.nn.functional.silu
 def fast_swiglu_inference(self, X):
     # gate = self.gate_proj(X)
     # up   = self.up_proj(X)
-    bsz, _, hd = X.shape
-    mlp_size = self.config.intermediate_size
-    temp = torch.empty((2, bsz, 1, mlp_size), dtype = X.dtype, device = "cuda")
+    # bsz, _, hd = X.shape
+    # mlp_size = self.config.intermediate_size
+    # temp = torch.empty((2, bsz, 1, mlp_size), dtype = X.dtype, device = "cuda")
 
-    gate = fast_linear_forward(self.gate_proj, X, out = temp[0])
-    up   = fast_linear_forward(self.  up_proj, X, out = temp[1])
+    gate = fast_linear_forward(self.gate_proj, X)#, out = temp[0])
+    up   = fast_linear_forward(self.  up_proj, X)#, out = temp[1])
     gate = torch_nn_functional_silu(gate, inplace = True)
     gate *= up
 
