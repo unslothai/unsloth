736786e05f851a6313262c24e571b6a8f1eba47d
jeromeku <jerome.ku@gmail.com>
2024-05-12 18:04:31 +0000

    add notes to test

--- tests/test_CEL.py
+++ tests/test_CEL.py
@@ -6,7 +6,7 @@ import torch
 from transformers.models.llama import LlamaConfig, LlamaForCausalLM
 
 import unsloth.utils.testing as test_utils
-from unsloth.kernels.fused_cel import patch_model as patch_model_fused_cel
+from unsloth.kernels.fused_cel import fused_cel_layer
 from unsloth.utils.memory import empty_cache
 
 torch.manual_seed(0)
@@ -20,54 +20,68 @@ def model_path():
     return MODEL_CONFIG_PATH
 
 
+def ref_cel(hidden_states, lm_head_weight, labels):
+    vocab_size = lm_head_weight.shape[0]
+    logits = hidden_states @ lm_head_weight.T
+    logits = logits.float()
+
+    shift_logits = logits[..., :-1, :].contiguous()
+    shift_labels = labels[..., 1:].contiguous()
+    loss_fct = torch.nn.CrossEntropyLoss()
+    shift_logits = shift_logits.view(-1, vocab_size)
+    shift_labels = shift_labels.view(-1)
+    shift_labels = shift_labels.to(shift_logits.device)
+    loss = loss_fct(shift_logits, shift_labels)
+
+    return loss
+
+
+def run_cel(fn, hidden_states, lm_head_weight, labels, **kwargs):
+    loss = fn(hidden_states, lm_head_weight, labels, **kwargs)
+    dX, dW = torch.autograd.grad(loss, [hidden_states, lm_head_weight])
+    return loss, dX, dW
+
+
+# Test will fail for dX when hidden_size > 4096 and n_loop_iters > 1 and vocab_size == 32000
+# Comment out the pytest.skip if you want to run
 @pytest.mark.parametrize("bs", [1])  # , 2, 4])
 @pytest.mark.parametrize("seqlen", [256])  # , 512, 1024])
-@pytest.mark.parametrize("hidden_size", [4096])
+@pytest.mark.parametrize("hidden_size", [128, 4096])
 @pytest.mark.parametrize(
     "vocab_size",
     [32000, 128256],  # , 256000]
 )  # llama-2, llama-3, gemma
 @pytest.mark.parametrize("dtype", ["float16", "bfloat16", "float32"])
-@pytest.mark.parametrize("n_loop_iters", [1, 2])  # , 2, 4])
-def test_cel(bs, seqlen, hidden_size, vocab_size, dtype, n_loop_iters, model_path):
+@pytest.mark.parametrize("n_loop_iters", [1, 2])  # , 2])  # , 2, 4])
+def test_cel(bs, seqlen, hidden_size, vocab_size, dtype, n_loop_iters):
     dtype = getattr(torch, dtype)
+    # if hidden_size >= 4096 and n_loop_iters > 1:
+    #     pytest.skip("Failure cases for dX")
+    # if not (hidden_size >= 4096 and n_loop_iters > 1 and vocab_size == 32000):
+    #     pytest.skip("Skipping, running only failure cases for dX")
 
-    model_config = LlamaConfig.from_pretrained(model_path)
-    model_config.update({"vocab_size": vocab_size})
-    model_config.update({"hidden_size": hidden_size})
-
-    model = LlamaForCausalLM(model_config).to(dtype).to("cuda")
-
-    # Mock LlamaModel.forward so that we can directly test the CEL loss and derivatives wrt the hidden states (input to the LM head)
     hidden_states = torch.randn(
         bs, seqlen, hidden_size, dtype=dtype, device="cuda", requires_grad=True
     )
-    model.model.forward = types.MethodType(
-        lambda *args, **kwargs: (hidden_states,), model.model
+
+    lm_head_weight = torch.randn(
+        (vocab_size, hidden_size), dtype=dtype, device="cuda", requires_grad=True
     )
 
     # Input ids aren't actually used, but we need to pass them to the model
-    input_ids = torch.randint(0, vocab_size, size=(bs, seqlen), device="cuda")
-    labels = input_ids.detach().clone()
-    attention_mask = torch.ones((bs, seqlen), device="cuda")
+    labels = torch.randint(0, vocab_size, size=(bs, seqlen), device="cuda")
 
     # Reference loss, dX, dW where dX is the gradients wrt to the hidden states and dW is the gradients wrt to the LM head weight
-    loss, *_ = model(
-        input_ids, labels=labels, attention_mask=attention_mask, return_dict=False
+    loss, dX, dW = run_cel(ref_cel, hidden_states, lm_head_weight, labels)
+    fused_loss, dX_fused, dW_fused = run_cel(
+        fused_cel_layer,
+        hidden_states,
+        lm_head_weight,
+        labels,
+        n_loop_iters=n_loop_iters,
+        ignore_index=-100,
+        reduction="mean",
     )
-    dX, dW = torch.autograd.grad(loss, [hidden_states, model.lm_head.weight])
-
-    # Patch the model to use fused CEL
-    fused_model = patch_model_fused_cel(
-        model, use_fused_cel=True, fused_cel_n_loop_iters=n_loop_iters
-    )
-    fused_loss, *_ = fused_model(
-        input_ids, labels=labels, attention_mask=attention_mask, return_dict=False
-    )
-    dX_fused, dW_fused = torch.autograd.grad(
-        fused_loss, [hidden_states, fused_model.lm_head.weight]
-    )
-
     if dtype == torch.bfloat16:
         atol, rtol = 1e-3, 1e-3  # Fails if < 1e-3
     elif dtype == torch.float16:
@@ -82,5 +96,70 @@ def test_cel(bs, seqlen, hidden_size, vocab_size, dtype, n_loop_iters, model_pat
         atol=atol,
         rtol=rtol,
     )
-    del fused_model
     empty_cache()
+
+
+# @pytest.mark.parametrize("bs", [1])  # , 2, 4])
+# @pytest.mark.parametrize("seqlen", [256])  # , 512, 1024])
+# @pytest.mark.parametrize("hidden_size", [4096])
+# @pytest.mark.parametrize(
+#     "vocab_size",
+#     [32000, 128256],  # , 256000]
+# )  # llama-2, llama-3, gemma
+# @pytest.mark.parametrize("dtype", ["float16", "bfloat16", "float32"])
+# @pytest.mark.parametrize("n_loop_iters", [1, 2])  # , 2, 4])
+# def test_cel(bs, seqlen, hidden_size, vocab_size, dtype, n_loop_iters, model_path):
+#     dtype = getattr(torch, dtype)
+
+#     model_config = LlamaConfig.from_pretrained(model_path)
+#     model_config.update({"vocab_size": vocab_size})
+#     model_config.update({"hidden_size": hidden_size})
+
+#     model = LlamaForCausalLM(model_config).to(dtype).to("cuda")
+
+#     # Mock LlamaModel.forward so that we can directly test the CEL loss and derivatives wrt the hidden states (input to the LM head)
+#     hidden_states = torch.randn(
+#         bs, seqlen, hidden_size, dtype=dtype, device="cuda", requires_grad=True
+#     )
+#     model.model.forward = types.MethodType(
+#         lambda *args, **kwargs: (hidden_states,), model.model
+#     )
+
+#     # Input ids aren't actually used, but we need to pass them to the model
+#     input_ids = torch.randint(0, vocab_size, size=(bs, seqlen), device="cuda")
+#     labels = input_ids.detach().clone()
+#     attention_mask = torch.ones((bs, seqlen), device="cuda")
+
+#     # Reference loss, dX, dW where dX is the gradients wrt to the hidden states and dW is the gradients wrt to the LM head weight
+#     loss, *_ = model(
+#         input_ids, labels=labels, attention_mask=attention_mask, return_dict=False
+#     )
+#     dX, dW = torch.autograd.grad(loss, [hidden_states, model.lm_head.weight])
+
+#     # Patch the model to use fused CEL
+#     fused_model = patch_model_fused_cel(
+#         model, use_fused_cel=True, fused_cel_n_loop_iters=n_loop_iters
+#     )
+#     fused_loss, *_ = fused_model(
+#         input_ids, labels=labels, attention_mask=attention_mask, return_dict=False
+#     )
+#     dX_fused, dW_fused = torch.autograd.grad(
+#         fused_loss, [hidden_states, fused_model.lm_head.weight]
+#     )
+
+#     if dtype == torch.bfloat16:
+#         atol, rtol = 1e-3, 1e-3  # Fails if < 1e-3
+#     elif dtype == torch.float16:
+#         atol, rtol = 1e-4, 1e-4  # Fails if < 1e-4
+#     else:
+#         atol, rtol = 1e-6, 1e-6
+
+#     test_utils.check_all(
+#         [loss, dX, dW],
+#         [fused_loss, dX_fused, dW_fused],
+#         ["loss", "dX", "dW"],
+#         atol=atol,
+#         rtol=rtol,
+#     )
+#     del fused_model
+#     empty_cache()
--- unsloth/kernels/fused_cel.py
+++ unsloth/kernels/fused_cel.py
@@ -1,17 +1,26 @@
 import logging
 import types
+from dataclasses import dataclass
 from math import sqrt
 from typing import List, Optional, Tuple, Union
 
-logger = logging.getLogger(__name__)
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 import triton
 import triton.language as tl
-from accelerate.utils.operations import convert_outputs_to_fp32
 from torch.nn import CrossEntropyLoss
 from transformers.modeling_outputs import CausalLMOutputWithPast
+from transformers.models.llama import LlamaForCausalLM
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class FusedCELConfig:
+    use_fused_cel: bool = True
+    n_loop_iters: int = 1
+    ignore_index: int = -100
+    reduction: str = "mean"
 
 
 @triton.jit
@@ -264,153 +273,275 @@ def fused_cel_linear(
     )
 
 
-# Only for debugging hugginfgface base models (not unsloth)
-def fused_cel_forward(
-    self,
-    input_ids: torch.LongTensor = None,
-    causal_mask: Optional[torch.Tensor] = None,  # this is deprecated
-    attention_mask: Optional[torch.Tensor] = None,
-    position_ids: Optional[torch.LongTensor] = None,
-    past_key_values: Optional[List[torch.FloatTensor]] = None,
-    inputs_embeds: Optional[torch.FloatTensor] = None,
-    labels: Optional[torch.LongTensor] = None,
-    use_cache: Optional[bool] = None,
-    output_attentions: Optional[bool] = None,
-    output_hidden_states: Optional[bool] = None,
-    return_dict: Optional[bool] = None,
-    cache_position: Optional[torch.LongTensor] = None,
-) -> Union[Tuple, CausalLMOutputWithPast]:
-    output_attentions = (
-        output_attentions
-        if output_attentions is not None
-        else self.config.output_attentions
-    )
-    output_hidden_states = (
-        output_hidden_states
-        if output_hidden_states is not None
-        else self.config.output_hidden_states
-    )
-    return_dict = (
-        return_dict if return_dict is not None else self.config.use_return_dict
-    )
-
-    # decoder outputs consists of (dec_features, layer_state, dec_hidden, dec_attn)
-    # if hasattr(self, "base_model"):
-    #     outputs = self.base_model.model.model(
-    #         input_ids=input_ids,
-    #         attention_mask=attention_mask,
-    #         position_ids=position_ids,
-    #         past_key_values=past_key_values,
-    #         inputs_embeds=inputs_embeds,
-    #         use_cache=use_cache,
-    #         output_attentions=output_attentions,
-    #         output_hidden_states=output_hidden_states,
-    #         return_dict=return_dict,
-    #         cache_position=cache_position,
-    #     )
-    # else:
-    outputs = self.model(
-        input_ids=input_ids,
-        attention_mask=attention_mask,
-        position_ids=position_ids,
-        past_key_values=past_key_values,
-        inputs_embeds=inputs_embeds,
-        use_cache=use_cache,
-        output_attentions=output_attentions,
-        output_hidden_states=output_hidden_states,
-        return_dict=return_dict,
-        cache_position=cache_position,
-    )
-
-    hidden_states = outputs[0]
-    if self.config.pretraining_tp > 1:
-        lm_head_slices = self.lm_head.weight.split(
-            self.vocab_size // self.config.pretraining_tp, dim=0
+def fused_cel_layer(
+    hidden_states,
+    lm_head_weight,
+    labels,
+    n_loop_iters=1,
+    ignore_index=-100,
+    reduction="mean",
+):
+    # if n_loop_iters > 1, we pad the labels with an extra ignore_index token
+    # so that we when chunking logits, we can divide the original number of tokens rather than number of tokens - 1
+    # This doesn't guarantee that the number of tokens (batch_size * seqlen) is a multiple of n_loop_iters
+    # but it makes simpler in the case for packed sequences, where the number of tokens is more often a multiple of 2
+    if n_loop_iters > 1:
+        labels = labels[..., 1:].contiguous()
+        # Pad labels
+        place_holder = torch.full(
+            (labels.shape[0], 1),
+            ignore_index,
+            dtype=labels.dtype,
+            device=labels.device,
         )
-        logits = [
-            F.linear(hidden_states, lm_head_slices[i])
-            for i in range(self.config.pretraining_tp)
-        ]
-        logits = torch.cat(logits, dim=-1)
-    elif self.config.use_fused_cel:
-        logger.warning_once(
-            "Using fused cross entropy loss, output logits will be in None"
+        labels = (
+            torch.hstack([labels, place_holder]).to(hidden_states.device).contiguous()
         )
-        assert labels is not None, "labels must not be None to use fused CEL"
 
-        # We don't shift when n_loop_iters >1 to make it easier to chunk along sequence length
-        # In this case, we don't shift hidden states or labels
-        # Instead, pass in full hidden states and append -100 (ignore_index) to labels
-        if self.config.fused_cel_n_loop_iters > 1:
-            labels = labels[..., 1:].contiguous()
-            place_holder = torch.full(
-                (labels.shape[0], 1),
-                self.config.fused_cel_ignore_index,
-                dtype=labels.dtype,
-                device=labels.device,
-            )
-            labels = (
-                torch.hstack([labels, place_holder])
-                .to(hidden_states.device)
-                .contiguous()
-            )
+        loss = fused_cel_linear(
+            hidden_states,
+            lm_head_weight,
+            labels,
+            n_loop_iters=n_loop_iters,
+            ignore_index=ignore_index,
+            reduction=reduction,
+        )
+    else:
+        # Need to shift, since kernel assumes labels and hidden states have same bs * seqlen
+        shift_hidden_states = hidden_states[
+            ..., :-1, :
+        ].contiguous()  # This is important -- MUST call contiguous, otherwise will cause downstream reshaping issues
+        shift_labels = labels[..., 1:].contiguous()
+        shift_labels = shift_labels.to(shift_hidden_states.device)
 
-            loss = fused_cel_linear(
+        loss = fused_cel_linear(
+            shift_hidden_states,
+            lm_head_weight,
+            shift_labels,
+            n_loop_iters=n_loop_iters,
+            ignore_index=ignore_index,
+            reduction=reduction,
+        )
+
+    return loss
+
+
+class LlamaForCausalLMFusedCEL(LlamaForCausalLM):
+    def forward(
+        self,
+        input_ids: torch.LongTensor = None,
+        causal_mask: Optional[torch.Tensor] = None,  # this is deprecated
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_values: Optional[List[torch.FloatTensor]] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        labels: Optional[torch.LongTensor] = None,
+        use_cache: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+        cache_position: Optional[torch.LongTensor] = None,
+        **kwargs,
+    ):
+        output_attentions = (
+            output_attentions
+            if output_attentions is not None
+            else self.config.output_attentions
+        )
+        output_hidden_states = (
+            output_hidden_states
+            if output_hidden_states is not None
+            else self.config.output_hidden_states
+        )
+        return_dict = (
+            return_dict if return_dict is not None else self.config.use_return_dict
+        )
+
+        outputs = self.model(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            past_key_values=past_key_values,
+            inputs_embeds=inputs_embeds,
+            use_cache=use_cache,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+            cache_position=cache_position,
+        )
+
+        hidden_states = outputs[0]
+        if self.config.pretraining_tp > 1:
+            lm_head_slices = self.lm_head.weight.split(
+                self.vocab_size // self.config.pretraining_tp, dim=0
+            )
+            logits = [
+                F.linear(hidden_states, lm_head_slices[i])
+                for i in range(self.config.pretraining_tp)
+            ]
+            logits = torch.cat(logits, dim=-1)
+        elif hasattr(self.config, "fused_cel") and self.config.fused_cel.use_fused_cel:
+            logger.warning_once(
+                "Using fused cross entropy loss, output logits will be in None"
+            )
+            assert labels is not None, "labels must not be None to use fused CEL"
+
+            loss = fused_cel_layer(
                 hidden_states,
                 self.lm_head.weight,
                 labels,
-                n_loop_iters=self.config.fused_cel_n_loop_iters,
-                ignore_index=self.config.fused_cel_ignore_index,
-                reduction=self.config.fused_cel_reduction,
+                n_loop_iters=self.config.fused_cel.n_loop_iters,
+                ignore_index=self.config.fused_cel.ignore_index,
+                reduction="mean",
             )
+
+            logits = None
+
         else:
-            # Need to shift, since kernel assumes labels and hidden states have same bs * seqlen
-            shift_hidden_states = hidden_states[
-                ..., :-1, :
-            ].contiguous()  # This is important -- MUST call contiguous, otherwise will cause downstream reshaping issues
-            shift_labels = labels[..., 1:].contiguous()
-            shift_labels = shift_labels.to(shift_hidden_states.device)
+            logits = self.lm_head(hidden_states)
+            logits = logits.float()
 
-            loss = fused_cel_linear(
-                shift_hidden_states,
-                self.lm_head.weight,
-                shift_labels,
-                n_loop_iters=self.config.fused_cel_n_loop_iters,
-                ignore_index=self.config.fused_cel_ignore_index,
-                reduction=self.config.fused_cel_reduction,
-            )
+            loss = None
+            if labels is not None:
+                # Shift so that tokens < n predict n
+                shift_logits = logits[..., :-1, :].contiguous()
+                # print("No fused shift logits", shift_logits.mean().item())
+                shift_labels = labels[..., 1:].contiguous()
+                # Flatten the tokens
+                loss_fct = CrossEntropyLoss()
+                shift_logits = shift_logits.view(-1, self.config.vocab_size)
+                shift_labels = shift_labels.view(-1)
+                # Enable model parallelism
+                shift_labels = shift_labels.to(shift_logits.device)
+                loss = loss_fct(shift_logits, shift_labels)
 
-        logits = None
+        if not return_dict:
+            output = (logits,) + outputs[1:]
+            return (loss,) + output if loss is not None else output
 
-    else:
-        logits = self.lm_head(hidden_states)
-        logits = logits.float()
+        return CausalLMOutputWithPast(
+            loss=loss,
+            logits=logits,
+            past_key_values=outputs.past_key_values,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
 
-        loss = None
-        if labels is not None:
-            # Shift so that tokens < n predict n
-            shift_logits = logits[..., :-1, :].contiguous()
-            # print("No fused shift logits", shift_logits.mean().item())
-            shift_labels = labels[..., 1:].contiguous()
-            # Flatten the tokens
-            loss_fct = CrossEntropyLoss()
-            shift_logits = shift_logits.view(-1, self.config.vocab_size)
-            shift_labels = shift_labels.view(-1)
-            # Enable model parallelism
-            shift_labels = shift_labels.to(shift_logits.device)
-            loss = loss_fct(shift_logits, shift_labels)
 
-    if not return_dict:
-        output = (logits,) + outputs[1:]
-        return (loss,) + output if loss is not None else output
+# Only for debugging hugginfgface base models (not unsloth)
+# def fused_cel_forward(
+#     self,
+#     input_ids: torch.LongTensor = None,
+#     causal_mask: Optional[torch.Tensor] = None,  # this is deprecated
+#     attention_mask: Optional[torch.Tensor] = None,
+#     position_ids: Optional[torch.LongTensor] = None,
+#     past_key_values: Optional[List[torch.FloatTensor]] = None,
+#     inputs_embeds: Optional[torch.FloatTensor] = None,
+#     labels: Optional[torch.LongTensor] = None,
+#     use_cache: Optional[bool] = None,
+#     output_attentions: Optional[bool] = None,
+#     output_hidden_states: Optional[bool] = None,
+#     return_dict: Optional[bool] = None,
+#     cache_position: Optional[torch.LongTensor] = None,
+# ) -> Union[Tuple, CausalLMOutputWithPast]:
+#     output_attentions = (
+#         output_attentions
+#         if output_attentions is not None
+#         else self.config.output_attentions
+#     )
+#     output_hidden_states = (
+#         output_hidden_states
+#         if output_hidden_states is not None
+#         else self.config.output_hidden_states
+#     )
+#     return_dict = (
+#         return_dict if return_dict is not None else self.config.use_return_dict
+#     )
 
-    return CausalLMOutputWithPast(
-        loss=loss,
-        logits=logits,
-        past_key_values=outputs.past_key_values,
-        hidden_states=outputs.hidden_states,
-        attentions=outputs.attentions,
-    )
+#     # decoder outputs consists of (dec_features, layer_state, dec_hidden, dec_attn)
+#     # if hasattr(self, "base_model"):
+#     #     outputs = self.base_model.model.model(
+#     #         input_ids=input_ids,
+#     #         attention_mask=attention_mask,
+#     #         position_ids=position_ids,
+#     #         past_key_values=past_key_values,
+#     #         inputs_embeds=inputs_embeds,
+#     #         use_cache=use_cache,
+#     #         output_attentions=output_attentions,
+#     #         output_hidden_states=output_hidden_states,
+#     #         return_dict=return_dict,
+#     #         cache_position=cache_position,
+#     #     )
+#     # else:
+#     outputs = self.model(
+#         input_ids=input_ids,
+#         attention_mask=attention_mask,
+#         position_ids=position_ids,
+#         past_key_values=past_key_values,
+#         inputs_embeds=inputs_embeds,
+#         use_cache=use_cache,
+#         output_attentions=output_attentions,
+#         output_hidden_states=output_hidden_states,
+#         return_dict=return_dict,
+#         cache_position=cache_position,
+#     )
+
+#     hidden_states = outputs[0]
+#     if self.config.pretraining_tp > 1:
+#         lm_head_slices = self.lm_head.weight.split(
+#             self.vocab_size // self.config.pretraining_tp, dim=0
+#         )
+#         logits = [
+#             F.linear(hidden_states, lm_head_slices[i])
+#             for i in range(self.config.pretraining_tp)
+#         ]
+#         logits = torch.cat(logits, dim=-1)
+#     elif self.config.use_fused_cel:
+#         logger.warning_once(
+#             "Using fused cross entropy loss, output logits will be in None"
+#         )
+#         assert labels is not None, "labels must not be None to use fused CEL"
+
+#         loss = fused_cel_layer(
+#             hidden_states,
+#             self.lm_head.weight,
+#             labels,
+#             n_loop_iters=self.config.n_loop_iters,
+#             ignore_index=self.config.ignore_index,
+#             reduction="mean",
+#         )
+
+#         logits = None
+
+#     else:
+#         logits = self.lm_head(hidden_states)
+#         logits = logits.float()
+
+#         loss = None
+#         if labels is not None:
+#             # Shift so that tokens < n predict n
+#             shift_logits = logits[..., :-1, :].contiguous()
+#             # print("No fused shift logits", shift_logits.mean().item())
+#             shift_labels = labels[..., 1:].contiguous()
+#             # Flatten the tokens
+#             loss_fct = CrossEntropyLoss()
+#             shift_logits = shift_logits.view(-1, self.config.vocab_size)
+#             shift_labels = shift_labels.view(-1)
+#             # Enable model parallelism
+#             shift_labels = shift_labels.to(shift_logits.device)
+#             loss = loss_fct(shift_logits, shift_labels)
+
+#     if not return_dict:
+#         output = (logits,) + outputs[1:]
+#         return (loss,) + output if loss is not None else output
+
+#     return CausalLMOutputWithPast(
+#         loss=loss,
+#         logits=logits,
+#         past_key_values=outputs.past_key_values,
+#         hidden_states=outputs.hidden_states,
+#         attentions=outputs.attentions,
+#     )
 
 
 def patch_model(
@@ -420,17 +551,16 @@ def patch_model(
     fused_cel_ignore_index=-100,
     fused_cel_reduction="mean",
 ):
-    model.config.update(
-        {
-            "use_fused_cel": use_fused_cel,
-            "fused_cel_n_loop_iters": fused_cel_n_loop_iters,
-            "fused_cel_ignore_index": fused_cel_ignore_index,
-            "fused_cel_reduction": fused_cel_reduction,
-        }
+    fused_config = FusedCELConfig(
+        use_fused_cel=use_fused_cel,
+        n_loop_iters=fused_cel_n_loop_iters,
+        ignore_index=fused_cel_ignore_index,
+        reduction=fused_cel_reduction,
     )
+    model.config.update({"fused_cel": fused_config})
 
     # if hasattr(model, "base_model"):
     #     model.base_model.model.forward = types.MethodType(fused_cel_forward, model)
     # else:
-    model.forward = types.MethodType(fused_cel_forward, model)
+    # model.forward = types.MethodType(fused_cel_forward, model)
     return model
