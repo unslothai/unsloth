e2850c07a4f77dce31ae688c97621adcd776744c
Daniel Han-Chen <danielhanchen@gmail.com>
2024-05-27 04:26:15 +1000

    Update tokenizer_utils.py

--- unsloth/tokenizer_utils.py
+++ unsloth/tokenizer_utils.py
@@ -834,7 +834,6 @@ def patch_sft_trainer_tokenizer():
 
     check_text = \
     "\n"\
-    "print('Fixing!')\n"
     "fix_untrained_tokens(self.model, self.tokenizer, self.train_dataset, eps = 1e-16)\n\n"
 
     check_text = check_text.split("\n")
