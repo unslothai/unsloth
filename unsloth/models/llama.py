761819763be3494056ba6a96db7b6c86208827bb
Daniel Han-Chen <danielhanchen@gmail.com>
2024-06-16 23:21:27 +1000

    Update llama.py

--- unsloth/models/llama.py
+++ unsloth/models/llama.py
@@ -1873,16 +1873,16 @@ class FastLlamaModel:
         
 
         # Patch tokenizer to pad to the right
-        # internal_model = model
-        # while hasattr(internal_model, "model"):
-        #     if hasattr(internal_model, "_saved_temp_tokenizer"):
-        #         internal_model._saved_temp_tokenizer.padding_side = "right"
-        #     pass
-        #     internal_model = internal_model.model
-        # pass
-        # if hasattr(internal_model, "_saved_temp_tokenizer"):
-        #     internal_model._saved_temp_tokenizer.padding_side = "right"
-        # pass
+        internal_model = model
+        while hasattr(internal_model, "model"):
+            if hasattr(internal_model, "_saved_temp_tokenizer"):
+                internal_model._saved_temp_tokenizer.padding_side = "right"
+            pass
+            internal_model = internal_model.model
+        pass
+        if hasattr(internal_model, "_saved_temp_tokenizer"):
+            internal_model._saved_temp_tokenizer.padding_side = "right"
+        pass
         # max_seq_length = model.max_seq_length
         # internal_model = model
         # while hasattr(internal_model, "model"):
@@ -1890,7 +1890,7 @@ class FastLlamaModel:
         #     internal_model = internal_model.model
         # pass
         # internal_model.max_seq_length = max_seq_length
-        # return model
+        return model
 
         # Clear deleted GPU items
         for _ in range(3):
