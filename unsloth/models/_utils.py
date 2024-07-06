632d0c2c4632634930c14c932cf4faa0ea68f413
Daniel Han <danielhanchen@gmail.com>
2024-07-06 15:32:21 -0700

    Update _utils.py

--- unsloth/models/_utils.py
+++ unsloth/models/_utils.py
@@ -365,15 +365,15 @@ def get_statistics():
         elif "\nINVOCATION_ID" in keynames: statistics = "lambda"
 
         if statistics is not None:
-            disabled = False
-            if not are_progress_bars_disabled():
-                disable_progress_bars()
-                disabled = True
-            pass
+            # disabled = False
+            # if not are_progress_bars_disabled():
+            #     disable_progress_bars()
+            #     disabled = True
+            # pass
             hf_hub_download(f"unslothai/statistics-{statistics}", "README.md", force_download = True)
-            if disabled:
-                enable_progress_bars()
-            pass
+            # if disabled:
+            #     enable_progress_bars()
+            # pass
         pass
     except:
         pass
