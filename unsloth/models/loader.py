1f8c053ccac00e2ff6692d059900f661c3850851
Daniel Han <danielhanchen@gmail.com>
2024-07-27 22:59:16 -0700

    Update loader.py

--- unsloth/models/loader.py
+++ unsloth/models/loader.py
@@ -82,7 +82,7 @@ def _get_new_mapper():
     new_mapper = new_mapper\
         .replace("INT_TO_FLOAT_MAPPER", "NEW_INT_TO_FLOAT_MAPPER")\
         .replace("FLOAT_TO_INT_MAPPER", "NEW_FLOAT_TO_INT_MAPPER")
-    exec(new_mapper, locals())
+    exec(new_mapper)
     return NEW_INT_TO_FLOAT_MAPPER, NEW_FLOAT_TO_INT_MAPPER
     # except:
     #     return {}, {}
