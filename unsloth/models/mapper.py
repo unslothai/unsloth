3843f0a226bfed80e3d936517c0e0b781e479225
Daniel Han <danielhanchen@gmail.com>
2024-07-27 23:03:29 -0700

    Update mapper.py

--- unsloth/models/mapper.py
+++ unsloth/models/mapper.py
@@ -218,10 +218,10 @@ __INT_TO_FLOAT_MAPPER = \
         "unsloth/Mistral-Nemo-Base-2407",
         "mistralai/Mistral-Nemo-Base-2407",
     ),
-    # "unsloth/Meta-Llama-3.1-8B-bnb-4bit" : (
-    #     "unsloth/Meta-Llama-3.1-8B",
-    #     "meta-llama/Meta-Llama-3.1-8B",
-    # ),
+    "unsloth/Meta-Llama-3.1-8B-bnb-4bit" : (
+        "unsloth/Meta-Llama-3.1-8B",
+        "meta-llama/Meta-Llama-3.1-8B",
+    ),
     "unsloth/Meta-Llama-3.1-8B-Instruct-bnb-4bit" : (
         "unsloth/Meta-Llama-3.1-8B-Instruct",
         "meta-llama/Meta-Llama-3.1-8B-Instruct",
