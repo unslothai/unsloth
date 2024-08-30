de8216c9d5ce477214f242c93a24b9f8256f2099
Alex Noble <Alex_Noble@hotmail.com>
2024-08-31 04:08:36 +0800

    Add fixed code to the trainer patcher.
    Add fixed code to the trainer patcher.

--- unsloth/tokenizer_utils.py
+++ unsloth/tokenizer_utils.py
@@ -1168,11 +1168,20 @@ def patch_sft_trainer_tokenizer():
         "    )\n"\
         "pass\n"\
         "import subprocess, re, gc, numpy as np\n"\
+        "import os\n"\
+        "index_for_cuda = -1\n"\
+        "if \"CUDA_VISIBLE_DEVICES\" in os.environ:\n"\
+        "    index_for_cuda = os.environ[\"CUDA_VISIBLE_DEVICES\"]\n"\
+        "    if \",\" in index_for_cuda:\n"\
+        "        raise RuntimeError(\"Unsloth currently does not support multi GPU setups - but we are working on it!\")\n"\
+        "    index_for_cuda = int(index_for_cuda)\n"\
         "a = np.array([0,])\n"\
         "try:\n"\
         "    a = subprocess.check_output('nvidia-smi --query-gpu=memory.used --format=csv', shell = True)\n"\
         "    a = re.findall(rb'([\\d]{1,})[\\s]{1,}M', a)\n"\
         "    a = np.array([int(x.decode('utf-8'))/1024 for x in a])\n"\
+        "    if index_for_cuda != -1:\n"\
+        "        a = np.array([output[index_for_cuda],])\n"\
         "except:\n"\
         "    if not torch.cuda.is_available():\n"\
         "        raise RuntimeError('Unsloth: We do not support AMD / Intel machines yet - it is a work in progress!')\n"\
