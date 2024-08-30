269c88230ec258d6590c626014edfad023ff6a7e
Alex Noble <Alex_Noble@hotmail.com>
2024-08-31 03:36:01 +0800

    Fix for multi gpu setup training with a single GPU.
    check_nvidia() originally spawns a new process for nvidia-smi, thus bypassing that GPU count might be limited by an OS environmental variable as this won't be reflected in the new process.
    
    Added check for if GPU is limited by OS environ, if multiple, raises error like original behaviour.
    
    If only one GPU enabled, only returns output for that GPU.

--- unsloth/tokenizer_utils.py
+++ unsloth/tokenizer_utils.py
@@ -1091,12 +1091,20 @@ pass
 
 
 def check_nvidia():
+    index_for_cuda = -1
+    if "CUDA_VISIBLE_DEVICES" in os.environ:
+        index_for_cuda = os.environ["CUDA_VISIBLE_DEVICES"]
+        if "," in index_for_cuda:
+            raise RuntimeError("Unsloth currently does not support multi GPU setups - but we are working on it!")
+        index_for_cuda = int(index_for_cuda)
     # Unsloth doesn't work yet on AMD devices - we're working on it!
     output = np.array([0,])
     try:
         output = subprocess.check_output("nvidia-smi --query-gpu=memory.used --format=csv", shell = True)
         output = re.findall(rb'([\d]{1,})[\s]{1,}M', output)
         output = np.array([int(x.decode('utf-8'))/1024 for x in output])
+        if index_for_cuda != -1:
+            output = np.array([output[index_for_cuda],])
     except:
         if not torch.cuda.is_available():
             raise RuntimeError("Unsloth: We do not support AMD / Intel machines yet - it is a work in progress!")
