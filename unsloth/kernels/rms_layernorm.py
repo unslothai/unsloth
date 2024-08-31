c76be22d0fbdb1d07d7dbbe005772dd0e5be6a6e
Daniel Han <danielhanchen@gmail.com>
2024-08-31 15:42:09 -0700

    Update rms_layernorm.py

--- unsloth/kernels/rms_layernorm.py
+++ unsloth/kernels/rms_layernorm.py
@@ -177,16 +177,22 @@ class Fast_RMS_Layernorm(torch.autograd.Function):
         Y = torch.empty((n_rows, n_cols), dtype = X.dtype, device = "cuda:0")
         r = torch.empty(n_rows, dtype = torch.float32, device = "cuda:0")
 
-        fx = _gemma_rms_layernorm_forward if gemma else _rms_layernorm_forward
-        fx[(n_rows,)](
-            Y, Y.stride(0),
-            X, X.stride(0),
-            W, W.stride(0),
-            r, r.stride(0),
-            n_cols, eps,
-            BLOCK_SIZE = BLOCK_SIZE,
-            num_warps  = num_warps,
-        )
+
+        row_var = X.to(torch.float32).square().sum(axis = 0) / n_cols
+        inv_var = torch.rqsrt(row_var + eps)
+        normed = X.to(torch.float32) * inv_var.unsqueeze(-1)
+        Y = normed * (W_row.unsqueeze(-1) + 1.0)
+
+        # fx = _gemma_rms_layernorm_forward if gemma else _rms_layernorm_forward
+        # fx[(n_rows,)](
+        #     Y, Y.stride(0),
+        #     X, X.stride(0),
+        #     W, W.stride(0),
+        #     r, r.stride(0),
+        #     n_cols, eps,
+        #     BLOCK_SIZE = BLOCK_SIZE,
+        #     num_warps  = num_warps,
+        # )
         ctx.eps = eps
         ctx.BLOCK_SIZE = BLOCK_SIZE
         ctx.num_warps  = num_warps
