5b4554ed5fd6b43e19c503ebb0d44847549d6b6f
Daniel Han <danielhanchen@gmail.com>
2024-10-14 19:08:49 -0700

    Update __init__.py

--- unsloth_zoo/__init__.py
+++ unsloth_zoo/__init__.py
@@ -14,4 +14,4 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-__version__ = "2024.10.0"
+__version__ = "2024.10.1"
