4c83f9b3387464195937ebe81742e4c3c99ee132
Rasmus Larsen <rasmus.larsen@alexandra.dk>
2024-10-17 09:39:34 +0000

    had accidentally deleted copyright notice

--- unsloth_zoo/dataset_utils.py
+++ unsloth_zoo/dataset_utils.py
@@ -1,4 +1,6 @@
 # Unsloth Zoo
+# Copyright (C) 2024-present the Unsloth AI team. All rights reserved.
+
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
 # by the Free Software Foundation, either version 3 of the License, or
