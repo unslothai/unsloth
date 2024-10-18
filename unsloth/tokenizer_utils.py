b187e3d9f2ee3ff38b47af26d23929d6016d7bcf
Daniel Han <danielhanchen@gmail.com>
2024-10-17 18:42:52 -0700

    Update tokenizer_utils.py

--- unsloth/tokenizer_utils.py
+++ unsloth/tokenizer_utils.py
@@ -935,11 +935,11 @@ def patch_sft_trainer_tokenizer():
         # Warn on gradient accumulation steps if it's used
         check_text += \
         "\n"\
-        "print(self.args.gradient_accumulation_steps)\n"\
         "if hasattr('self', 'args') and hasattr('self.args', 'gradient_accumulation_steps'):\n"\
         "    gradient_accumulation_steps = self.args.gradient_accumulation_steps\n"\
         "    if type(gradient_accumulation_steps) is int and gradient_accumulation_steps > 1:\n"\
         "        from transformers import __version__ as transformers_version\n"\
+        "        print(1)\n"\
         "        if Version(transformers_version) <= Version('4.45.2'):\n"\
         "            print('Unsloth: Please use our fixed gradient_accumulation_steps by updating transformers and Unsloth!')\n"\
         "\n\n"
