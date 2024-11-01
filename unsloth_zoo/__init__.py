8bccfe462511aa7dfc00e32c34b59638bf6d1aa8
Daniel Han <danielhanchen@gmail.com>
2024-11-01 01:58:53 -0700

    Update __init__.py

--- unsloth_zoo/__init__.py
+++ unsloth_zoo/__init__.py
@@ -4,4 +4,4 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-__version__ = "2024.10.5"
+__version__ = "2024.11.0"
