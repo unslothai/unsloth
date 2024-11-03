a8115330de9a4a811de135f73222302012d9a670
Daniel Han <danielhanchen@gmail.com>
2024-11-03 00:23:26 -0700

    Update __init__.py

--- unsloth_zoo/__init__.py
+++ unsloth_zoo/__init__.py
@@ -4,4 +4,24 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-__version__ = "2024.10.5"
+__version__ = "2024.11.1"
+
+import importlib.util
+if importlib.util.find_spec("unsloth") is None:
+    raise ImportError("Please install Unsloth via `pip install unsloth`!")
+pass
+del importlib.util
+
+import os
+if not ("UNSLOTH_IS_PRESENT" in os.environ):
+    raise ImportError("Please install Unsloth via `pip install unsloth`!")
+pass
+
+try:
+    print("🦥 Unsloth: Will patch your computer to enable 2x faster free finetuning.")
+except:
+    print("Unsloth: Will patch your computer to enable 2x faster free finetuning.")
+pass
+# Log Unsloth-Zoo Utilities
+os.environ["UNSLOTH_ZOO_IS_PRESENT"] = "1"
+del os
