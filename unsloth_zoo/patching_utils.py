a0a053e130837d7284d6343d92fa9c8c1d02dba9
Daniel Han <danielhanchen@gmail.com>
2024-11-06 02:10:33 -0800

    Update patching_utils.py

--- unsloth_zoo/patching_utils.py
+++ unsloth_zoo/patching_utils.py
@@ -16,13 +16,11 @@
 
 import torch
 import os
-from packaging.version import Version
 
 __all__ = [
     "patch_compiling_bitsandbytes",
     "patch_layernorm",
     "patch_torch_compile",
-    "patch_regional_compilation",
     "patch_model_and_tokenizer",
 ]
 
@@ -123,27 +121,6 @@ def patch_torch_compile(debug = True, O3 = False, ignore_errors = False):
         try:    exec(_try_dynamo_argument)
         except: pass
     pass
-pass
-
-
-def patch_regional_compilation():
-    # Regional torch 2.5 Recompilation - weirdly very slow??
-    if torch.nn.ModuleList.__name__ == "UnslothModuleList": return
-    # Only works for torch 2.5
-    if Version(torch.__version__) < Version("2.5.0"): return
-
-    old_module_list = torch.nn.ModuleList
-    os.environ["UNSLOTH_PATCHED"] = "1"
-
-    def UnslothModuleList(*args, **kwargs):
-        if len(args) == 1 and len(kwargs) == 0 and type(args[0]) is list:
-            args = [old_module_list([torch.compile(x, dynamic = True, options = torch_compile_options, fullgraph = False) for x in args[0]])]
-        return old_module_list(*args, **kwargs)
-    pass
-    UnslothModuleList.__doc__ = old_module_list.__doc__
-
-    torch.nn.ModuleList = UnslothModuleList
-    return
 pass
 
 
