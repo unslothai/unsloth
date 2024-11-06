296b42ad88140c4fe5ae2b06f68766e8c0423dfa
Daniel Han <danielhanchen@gmail.com>
2024-11-05 16:34:37 -0800

    Update gradient_checkpointing.py

--- unsloth_zoo/gradient_checkpointing.py
+++ unsloth_zoo/gradient_checkpointing.py
@@ -222,6 +222,8 @@ def patch_unsloth_gradient_checkpointing():
     if torch.utils.checkpoint.checkpoint.__name__ == "unsloth_offloaded_gradient_checkpoint": return
     torch.utils.checkpoint._old_checkpoint = torch.utils.checkpoint.checkpoint
     torch.utils.checkpoint.checkpoint = unsloth_offloaded_gradient_checkpoint
+    import transformers.modeling_utils
+    transformers.modeling_utils.checkpoint = unsloth_offloaded_gradient_checkpoint
     os.environ["UNSLOTH_PATCHED"] = "1"
 pass
 
@@ -232,6 +234,8 @@ def patch_gradient_checkpointing():
     if torch.utils.checkpoint.checkpoint.__name__ == "unsloth_gradient_checkpoint": return
     torch.utils.checkpoint._old_checkpoint = torch.utils.checkpoint.checkpoint
     torch.utils.checkpoint.checkpoint = unsloth_gradient_checkpoint
+    import transformers.modeling_utils
+    transformers.modeling_utils.checkpoint = unsloth_gradient_checkpoint
     os.environ["UNSLOTH_PATCHED"] = "1"
 pass
 
