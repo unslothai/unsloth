c381b6067627bf2f4b1b6505fb95ed0c47dc51dd
Daniel Han <danielhanchen@gmail.com>
2024-11-15 22:23:31 -0800

    Update patching_utils.py

--- unsloth_zoo/patching_utils.py
+++ unsloth_zoo/patching_utils.py
@@ -96,8 +96,8 @@ def patch_torch_compile(debug = True, O3 = False, ignore_errors = True):
         "config.coordinate_descent_tuning = True",
         f"config.aggressive_fusion = {O3}", # Careful changes results!
         # [TODO] COMBO KERNELS makes everything slower!
-        # "config.combo_kernels = True", # Experimental - enable the combo kernel that combines data-independent kernels
-        # "config.combo_kernel_foreach_dynamic_shapes = True",
+        "config.combo_kernels = True", # Experimental - enable the combo kernel that combines data-independent kernels
+        "config.combo_kernel_foreach_dynamic_shapes = True",
         "config.freezing = False", # Freezes weights --> ** only useful for inference **
         f"config.triton.multi_kernel = {O3}", # use tuning to pick between different subkernels
         "config.cuda.enable_cuda_lto = True",
