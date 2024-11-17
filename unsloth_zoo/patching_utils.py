117183b6bed78d3eabfc41fcba661275f4486d96
Daniel Han <danielhanchen@gmail.com>
2024-11-16 22:29:55 -0800

    Update patching_utils.py

--- unsloth_zoo/patching_utils.py
+++ unsloth_zoo/patching_utils.py
@@ -24,6 +24,10 @@ __all__ = [
     "patch_model_and_tokenizer",
 ]
 
+global UNSLOTH_COMPILE_LOCATION
+UNSLOTH_COMPILE_LOCATION = "unsloth_compiled_cache"
+
+
 # Also disable compiling on bitsandbytes
 def patch_compiling_bitsandbytes():
     # peft.tuners.lora.bnb.Linear8bitLt.forward = \
@@ -76,6 +80,11 @@ def patch_torch_compile(debug = True, O3 = False, ignore_errors = True):
         torch._dynamo.config.verbose = False
     pass
     os.environ["UNSLOTH_PATCHED"] = "1"
+    # See https://pytorch.org/tutorials/recipes/torch_compile_caching_tutorial.html
+    # Caches kernel generations for faster restarts
+    os.environ["TORCHINDUCTOR_FX_GRAPH_CACHE"] = "1"
+    os.environ["TORCHINDUCTOR_AUTOTUNE_REMOTE_CACHE"] = "1"
+    os.environ["TORCHINDUCTOR_CACHE_DIR"] = UNSLOTH_COMPILE_LOCATION
 
     # Torch compile arguments
     torch_compile_arguments = [
