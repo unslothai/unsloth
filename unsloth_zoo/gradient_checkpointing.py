dca3e70c86609b780e9c0729c1d8a2285be93e87
Daniel Han <danielhanchen@gmail.com>
2024-11-17 17:20:22 -0800

    Update gradient_checkpointing.py

--- unsloth_zoo/gradient_checkpointing.py
+++ unsloth_zoo/gradient_checkpointing.py
@@ -325,7 +325,7 @@ class UnslothCheckpointFunction(torch.autograd.Function):
                 pointer = arg.data_ptr()
                 if pointer in CHECKPOINT_MAPPING:
                     buffer = CHECKPOINT_BUFFERS[CHECKPOINT_MAPPING[pointer]]
-                    tensor_inputs.append(buffer.view(arg.shape, dtype = arg.dtype))
+                    tensor_inputs.append(buffer.view(dtype = arg.dtype, size = arg.shape))
                 else:
                     array = CHECKPOINT_BUFFERS[CHECKPOINT_INDEX]
                     if arg.dtype == array.dtype:
