1edabb26d7ad8d91f6586bc22f90a77c2387848f
Daniel Han <danielhanchen@gmail.com>
2024-11-17 20:34:56 -0800

    Update compiler.py

--- unsloth_zoo/compiler.py
+++ unsloth_zoo/compiler.py
@@ -36,7 +36,25 @@ UNSLOTH_CREATED_FUNCTIONS = []
 
 
 _disabled_sdpa_code = """
+# Unsloth Zoo - Utilities for Unsloth
+# Copyright 2023-present Daniel Han-Chen & the Unsloth team. All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import torch
+from unsloth_zoo.loss_utils import fused_linear_cross_entropy
+
 scaled_dot_product_attention = torch.nn.functional.scaled_dot_product_attention
 @torch.compiler.disable(recursive = False)
 def disable_compile_scaled_dot_product_attention(*args, **kwargs):
@@ -291,6 +309,18 @@ def unsloth_compile_transformers(
     modeling_file = eval(model_location)
     if hasattr(modeling_file, "__UNSLOTH_PATCHED__"): return
 
+    # torch_compile_options
+    UNSLOTH_COMPILE_DEBUG         = os.environ.get("UNSLOTH_COMPILE_DEBUG",         "0") == "1"
+    UNSLOTH_COMPILE_MAXIMUM       = os.environ.get("UNSLOTH_COMPILE_MAXIMUM",       "0") == "1"
+    UNSLOTH_COMPILE_IGNORE_ERRORS = os.environ.get("UNSLOTH_COMPILE_IGNORE_ERRORS", "0") == "1"
+    torch_compile_options = {
+        "epilogue_fusion"   : True,
+        "max_autotune"      : True,
+        "shape_padding"     : True,
+        "trace.enabled"     : UNSLOTH_COMPILE_DEBUG,
+        "triton.cudagraphs" : False,
+    }
+    
     modeling_file.__UNSLOTH_PATCHED__ = True
     functions = dir(modeling_file)
     full_source = inspect.getsource(modeling_file)
@@ -597,7 +627,7 @@ def unsloth_compile_transformers(
     except:
         raise RuntimeError('Unsloth currently does not support multi GPU setups - but we are working on it!')
     pass
-    
+
     import transformers.trainer
     items_in_trainer = dir(transformers.trainer)
     good_items = []
