7150c52bc0cc963b9b6394a03e269186fada20ba
Daniel Han <danielhanchen@gmail.com>
2024-11-19 00:37:39 -0800

    Update gradient_checkpointing.py

--- unsloth_zoo/gradient_checkpointing.py
+++ unsloth_zoo/gradient_checkpointing.py
@@ -330,25 +330,23 @@ class UnslothCheckpointFunction(torch.autograd.Function):
                 else:
                     done = True
                     saved_arg = arg.to("cpu", non_blocking = True)
-                        if CHECKPOINT_LOGGING:
-                            CHECKPOINT_LOGGING = False
-                            try:
-                                print("🦥 Unsloth: Smart Gradient Checkpointing turned on")
-                            except:
-                                print("Unsloth: Smart Gradient Checkpointing turned on")
-                            pass
+                    if CHECKPOINT_LOGGING:
+                        CHECKPOINT_LOGGING = False
+                        try:
+                            print("🦥 Unsloth: Smart Gradient Checkpointing turned on")
+                        except:
+                            print("Unsloth: Smart Gradient Checkpointing turned on")
                         pass
-                        old_size = array.numel()
-                        new_size = arg.numel()
-                        # if new_size > old_size:
-                        #     CHECKPOINT_BUFFERS[CHECKPOINT_INDEX].resize_(new_size)
-                        #     array = CHECKPOINT_BUFFERS[CHECKPOINT_INDEX]
-                        # array = array[:new_size].view(shape)
-                        # array.copy_(arg, non_blocking = True)
-                        tensor_inputs.append(arg.to("cpu", non_blocking = True))
-                        CHECKPOINT_INDEX += 1
-                    else:
-                        tensor_inputs.append(arg)
+                    pass
+                    old_size = array.numel()
+                    new_size = arg.numel()
+                    # if new_size > old_size:
+                    #     CHECKPOINT_BUFFERS[CHECKPOINT_INDEX].resize_(new_size)
+                    #     array = CHECKPOINT_BUFFERS[CHECKPOINT_INDEX]
+                    # array = array[:new_size].view(shape)
+                    # array.copy_(arg, non_blocking = True)
+                    tensor_inputs.append(arg.to("cpu", non_blocking = True))
+                    CHECKPOINT_INDEX += 1
                 pass
                 ctx.tensor_indices.append(i)
                 ctx.inputs.append(None)
