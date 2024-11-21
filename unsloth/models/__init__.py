65c7c76e26c8ed76de4718c39497e1492f4ab285
Erland366 <erland.pg366@gmail.com>
2024-11-21 20:14:11 +0400

    Add PatchKTOTrainer and update model imports

--- unsloth/models/__init__.py
+++ unsloth/models/__init__.py
@@ -17,4 +17,5 @@ from .llama   import FastLlamaModel
 from .mistral import FastMistralModel
 from .qwen2   import FastQwen2Model
 from .dpo     import PatchDPOTrainer
+from .kto     import PatchKTOTrainer
 from ._utils  import is_bfloat16_supported
