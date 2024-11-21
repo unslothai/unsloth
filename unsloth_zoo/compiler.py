cfe603925f9d29c0abf676529b37fc736e106e3e
Daniel Han <danielhanchen@gmail.com>
2024-11-21 06:22:06 -0800

    Update compiler.py

--- unsloth_zoo/compiler.py
+++ unsloth_zoo/compiler.py
@@ -338,14 +338,17 @@ loss = loss_fct(shift_logits, shift_labels)
 """
 
 cross_entropy_replacement_1 = """
-n_items = loss_kwargs.get("num_items_in_batch", None) or loss_kwargs.get("n_items", None)
-loss = fused_linear_cross_entropy(
-    hidden_states      = hidden_states,
-    lm_weight          = self.lm_head.weight,
-    labels             = labels,
-    num_items_in_batch = n_items,
-    logit_softcapping  = getattr(self.config, "final_logit_softcapping", 0),
-)
+if self.training and labels is not None:
+    n_items = loss_kwargs.get("num_items_in_batch", None) or loss_kwargs.get("n_items", None)
+    loss = fused_linear_cross_entropy(
+        hidden_states      = hidden_states,
+        lm_weight          = self.lm_head.weight,
+        labels             = labels,
+        num_items_in_batch = n_items,
+        logit_softcapping  = getattr(self.config, "final_logit_softcapping", 0),
+    )
+else:
+    logits = self.lm_head(hidden_states)
 """
 
 cross_entropy_find_2 = """
