41e6f94c030c3c245691d2d7513d82134743e45b
Daniel Han <danielhanchen@gmail.com>
2024-11-26 03:07:50 -0800

    Update loader.py

--- unsloth/models/loader.py
+++ unsloth/models/loader.py
@@ -448,32 +448,32 @@ class FastVisionModel(FastBaseVisionModel):
 
         if not was_disabled: enable_progress_bars()
 
-        # with contextlib.redirect_stdout(open(os.devnull, "w")):
-        patch_loss_functions(torch_compile = False)
-        model_types = unsloth_compile_transformers(
-            model_name              = model_name,
-            sdpa_dynamic_mask       = True,
-            sdpa_bool_masks         = True,
-            sdpa_gqa_replace        = True,
-            sdpa_dynamic_compile    = True,
-            compile_attention       = True,
-            disable_causal_masks    = True,
-            compile_torch_modules   = True,
-            compile_custom_modules  = True,
-            compile_function_calls  = True,
-            fuse_lm_head            = True,
-            gradient_checkpointing  = True,
-            manual_replacements     = True,
-            epilogue_fusion         = True,
-            max_autotune            = False,
-            shape_padding           = True,
-            cudagraphs              = False,
-            debug                   = False,
-            import_from_cache       = False,
-            disable                 = False,
-            return_logits           = return_logits,
-        )
-        # pass
+        with contextlib.redirect_stdout(open(os.devnull, "w")):
+            patch_loss_functions(torch_compile = False)
+            model_types = unsloth_compile_transformers(
+                model_name              = model_name,
+                sdpa_dynamic_mask       = True,
+                sdpa_bool_masks         = True,
+                sdpa_gqa_replace        = True,
+                sdpa_dynamic_compile    = True,
+                compile_attention       = True,
+                disable_causal_masks    = True,
+                compile_torch_modules   = True,
+                compile_custom_modules  = True,
+                compile_function_calls  = True,
+                fuse_lm_head            = True,
+                gradient_checkpointing  = True,
+                manual_replacements     = True,
+                epilogue_fusion         = True,
+                max_autotune            = False,
+                shape_padding           = True,
+                cudagraphs              = False,
+                debug                   = False,
+                import_from_cache       = False,
+                disable                 = False,
+                return_logits           = return_logits,
+            )
+        pass
 
         # Check if this is local model since the tokenizer gets overwritten
         if  os.path.exists(os.path.join(old_model_name, "tokenizer_config.json")) and \
