815576e6b1715bf994b7e6f76c3c6e46bc57f958
Daniel Han <danielhanchen@gmail.com>
2024-11-25 21:59:40 -0800

    Update _utils.py

--- unsloth/models/_utils.py
+++ unsloth/models/_utils.py
@@ -1178,8 +1178,8 @@ LOGITS_ERROR_STRING = \
 def raise_logits_error(*args, **kwargs): raise NotImplementedError(LOGITS_ERROR_STRING)
 class EmptyLogits:
     def __init__(self): return
-    __getitem__ = raise_logits_error
-    __getattr__ = raise_logits_error
+    # __getitem__ = raise_logits_error
+    # __getattr__ = raise_logits_error
     def __repr__(self): return LOGITS_ERROR_STRING
     def __str__ (self): return LOGITS_ERROR_STRING
 pass
