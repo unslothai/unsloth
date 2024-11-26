190a533b8670ce8c2c3c859c07560c8e21425584
Daniel Han <danielhanchen@gmail.com>
2024-11-26 14:20:54 -0800

    Update __init__.py

--- unsloth/models/__init__.py
+++ unsloth/models/__init__.py
@@ -16,6 +16,5 @@ from .loader  import FastLanguageModel, FastVisionModel
 from .llama   import FastLlamaModel
 from .mistral import FastMistralModel
 from .qwen2   import FastQwen2Model
-from .dpo     import PatchDPOTrainer
-from .kto     import PatchKTOTrainer
+from .dpo     import PatchDPOTrainer, PatchKTOTrainer
 from ._utils  import is_bfloat16_supported
