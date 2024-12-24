1d04daaad8d8c637f4828db41ae4bb8e21c623b0
Daniel Han <danielhanchen@gmail.com>
2024-12-24 00:18:47 -0800

    Update gemma2.py

--- unsloth/models/gemma2.py
+++ unsloth/models/gemma2.py
@@ -169,7 +169,7 @@ pass
 def Gemma2DecoderLayer_fast_forward(
     self,
     hidden_states:        torch.Tensor,
-    causal_mask:          Optional[xformers.attn_bias.BlockDiagonalCausalMask] = None,
+    causal_mask:          Optional[BlockDiagonalCausalMask] = None,
     attention_mask:       Optional[torch.Tensor] = None,
     position_ids:         Optional[torch.LongTensor] = None,
     past_key_value:       Optional[Tuple[torch.Tensor]] = None,
