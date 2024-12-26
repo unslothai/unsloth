f69f88e84131280f860f88b8ce524d424ddc254f
Daniel Han <danielhanchen@gmail.com>
2024-12-26 04:05:07 -0800

    Update _utils.py

--- unsloth/models/_utils.py
+++ unsloth/models/_utils.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "2024.12.10"
+__version__ = "2024.12.11"
 
 __all__ = [
     "prepare_model_for_kbit_training",
