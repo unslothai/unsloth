43ef5c635fd5a8385c96510b4c8bff33f4614d8d
Daniel Han <danielhanchen@gmail.com>
2024-12-26 22:25:19 -0800

    Update _utils.py

--- unsloth/models/_utils.py
+++ unsloth/models/_utils.py
@@ -1021,7 +1021,7 @@ def _unsloth_get_batch_samples(self, epoch_iterator, num_batches):
     if torch.is_tensor(num_items_in_batch):
         num_items_in_batch = num_items_in_batch.item()
 
-    print("NUM_ITMES = ", num_items_in_batch, type(batch_samples))
+    print("NUM_ITMES = ", num_items_in_batch, type(batch_samples), self.model)
 
     return batch_samples, num_items_in_batch
 
