2ad2932a47948ec8f262aa1715a3e52db864fdb5
Daniel Han <danielhanchen@gmail.com>
2024-12-29 00:49:01 -0800

    Update compiler.py

--- unsloth_zoo/compiler.py
+++ unsloth_zoo/compiler.py
@@ -875,7 +875,7 @@ def unsloth_compile_transformers(
     import os
     for x, value in arguments.items():
         print(x, value)
-        exec(f"{x} = {x} or (os.environ.get('UNSLOTH_COMPILE_{x.upper()}', '0') == '1')", locals())
+        exec(f"{x} = {x} or (os.environ.get('UNSLOTH_COMPILE_{x.upper()}', '0') == '1')")
         print(x, eval(x), eval(f"os.environ.get('UNSLOTH_COMPILE_{x.upper()}', '0') == '1'"))
     UNSLOTH_RETURN_LOGITS = return_logits
     UNSLOTH_FULLGRAPH     = fullgraph
