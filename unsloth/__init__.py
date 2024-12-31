c83f5422a1abddaa08454960716a9e17e3507e3d
Daniel Han <danielhanchen@gmail.com>
2024-12-31 00:38:18 -0800

    Update __init__.py

--- unsloth/__init__.py
+++ unsloth/__init__.py
@@ -58,7 +58,7 @@ pass
 # And optimize pinning of memory
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = \
     "expandable_segments:True,"\
-    "roundup_power2_divisions:[32:256,64:128,256:64,>:32],"\
+    "roundup_power2_divisions:[64:128,256:64,>:32],"\
     "pinned_use_cuda_host_register:True,"\
     "pinned_num_register_threads:8"
 
