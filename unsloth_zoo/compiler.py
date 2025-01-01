ec39da04407eff33267054cca65aa485e139733c
Daniel Han <danielhanchen@gmail.com>
2024-12-31 19:51:43 -0800

    Update compiler.py

--- unsloth_zoo/compiler.py
+++ unsloth_zoo/compiler.py
@@ -642,14 +642,14 @@ COMPILED_LORA_FORWARD = """
 @torch.compile(fullgraph = False, dynamic = None, options = torch_compile_options)
 def lora_forward(result, lora_A, lora_B, dropout, x, scaling):
     xA = dropout(x) @ lora_A.weight.t()
-    # output = result + scaling * xA @ lora_B.weight.t()
-    output = torch.addmm(
-        result.reshape(-1, result.shape[-1]),
-        xA.reshape(-1, xA.shape[-1]),
-        lora_B.weight.t(),
-        alpha = scaling,
-        beta = 1,
-    ).reshape(result.shape)
+    output = result + scaling * xA @ lora_B.weight.t()
+    # output = torch.addmm(
+    #     result.reshape(-1, result.shape[-1]),
+    #     xA.reshape(-1, xA.shape[-1]),
+    #     lora_B.weight.t(),
+    #     alpha = scaling,
+    #     beta = 1,
+    # ).reshape(result.shape)
 
     bias = lora_B.bias
     if bias is not None:
@@ -857,7 +857,7 @@ def unsloth_compile_transformers(
 ):
     # Code licensed under LGPL
     if disable: return
-    fast_lora_forwards = False
+    fast_lora_forwards = True
     # fast_residual_stream = True
     import_from_cache = False
 
