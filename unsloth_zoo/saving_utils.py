9be0c98cce5fb647b2c16c4f6cb7de74e2c100b6
Daniel Han <danielhanchen@gmail.com>
2025-01-06 01:14:55 -0800

    Saving, llama.cpp

--- unsloth_zoo/saving_utils.py
+++ unsloth_zoo/saving_utils.py
@@ -188,7 +188,8 @@ pass
 
 def assert_same_keys(model, new_state_dict):
     # All Unsloth Zoo code licensed under LGPLv3
-    original_keys = model.base_model.model.state_dict().keys()
+    inner_model = model.base_model.model if hasattr(model, "base_model") else model
+    original_keys = inner_model.state_dict().keys()
     all_original_keys = set()
     for x in original_keys:
         where_weight = x.rfind(".weight")
@@ -223,9 +224,9 @@ def create_lora_statistics(model, merge_into_original = False, return_state_dict
 
     remove_keys = set()
     keep_keys   = set()
-    assert(hasattr(model, "base_model"))
-    assert(hasattr(model.base_model, "model"))
-    for name, module in model.base_model.model.named_modules():
+
+    inner_model = model.base_model.model if hasattr(model, "base_model") else model
+    for name, module in inner_model.named_modules():
         if name == "": continue
 
         elif name.endswith(".lora_A.default"):
@@ -277,7 +278,7 @@ def create_lora_statistics(model, merge_into_original = False, return_state_dict
 
     # Also return state_dict if needed
     if return_state_dict:
-        old_state_dict = model.base_model.model.state_dict()
+        old_state_dict = inner_model.state_dict()
         state_dict     = collections.OrderedDict()
         for name, param in old_state_dict.items():
 
@@ -497,8 +498,7 @@ def merge_and_overwrite_lora(
 ):
     # All Unsloth Zoo code licensed under LGPLv3
     # Directly downloads 16bit original weights and merges LoRA
-    if not hasattr(model, "base_model"):
-        raise RuntimeError("Unsloth: This is not a LoRA model - please save normally!")
+    inner_model = model.base_model.model if hasattr(model, "base_model") else model
 
     try:
         model_name = get_model_name(model.config._name_or_path, load_in_4bit = False)
@@ -555,7 +555,7 @@ def merge_and_overwrite_lora(
 
     # Save config / generation_config via no state_dict and tokenizer
     if tokenizer is not None: tokenizer.save_pretrained(save_directory = save_directory,)
-    model.base_model.model.save_pretrained(
+    inner_model.save_pretrained(
         save_directory = save_directory,
         state_dict = {},
     )
@@ -729,6 +729,8 @@ def merge_and_dequantize_lora(
 ):
     # All Unsloth Zoo code licensed under LGPLv3
     # Dequantizes model to 16bit weights and merges LoRA
+    inner_model = model.base_model.model if hasattr(model, "base_model") else model
+
     (
         username, repo_id, hf_api, token,
         output_dtype, element_size,
@@ -823,7 +825,7 @@ def merge_and_dequantize_lora(
         save_directory,
     )
     save_pretrained_dequantized(
-        model.base_model.model,
+        inner_model,
         save_directory     = save_directory,
         push_to_hub        = False,
         max_shard_size     = max_shard_size_in_bytes,
