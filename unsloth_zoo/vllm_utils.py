64b694f2bc0327d3ed7f5f87e46a733c4bec1b29
Daniel Han <danielhanchen@gmail.com>
2025-02-03 20:34:32 -0800

    Update vllm_utils.py

--- unsloth_zoo/vllm_utils.py
+++ unsloth_zoo/vllm_utils.py
@@ -16,9 +16,20 @@
 
 __all__ = [
     "patch_vllm",
+    "vllm_dynamic_quant_supported",
+    "convert_vllm_to_huggingface",
+    "get_vllm_state_dict",
+    "assert_same_state_dict",
 ]
 from typing import Optional, List, Tuple, Dict, Any
 from transformers.utils.import_utils import _is_package_available
+import re
+from collections import OrderedDict
+import numpy as np
+from transformers import AutoModelForCausalLM
+from copy import deepcopy
+from .utils import _get_dtype
+
 
 if _is_package_available("vllm"):
 
@@ -180,6 +191,332 @@ def patch_vllm():
     patch_vllm_bitsandbytes()
 pass
 
+
+def vllm_dynamic_quant_supported(
+    model_name,
+    config,
+) -> bool:
+    # All Unsloth Zoo code licensed under LGPLv3
+
+    # Check if vLLM supports some Unsloth dynamic quants
+    # Sometimes we quantize modules within a layer, but not an entire layer
+    # If so, then we cannot use dynamic quants for now
+    if not model_name.lower().endswith("unsloth-bnb-4bit"): return True
+    if "quantization_config" not in config: return True
+
+    llm_int8_skip_modules = config.quantization_config.get("llm_int8_skip_modules", {})
+    
+    # Only allow layer modules ie model.layers.1.mlp or model.layers.1.self_attn
+    
+    # Exclude model.layers.27.mlp.gate_proj
+    parent_llm_int8_skip_modules = []
+    for module in llm_int8_skip_modules:
+        # $ means end of string
+        if re.search(r"[\d]\.[^\.]{1,}$", module) or "." not in module:
+            parent_llm_int8_skip_modules.append(module)
+    pass
+
+    parent_llm_int8_skip_modules = set(parent_llm_int8_skip_modules)
+    find_regex = "|".join(re.escape(x) for x in parent_llm_int8_skip_modules)
+    find_regex = re.compile(find_regex)
+
+    for module in llm_int8_skip_modules:
+        # Could not find parent
+        if find_regex.search(module) is None: return False
+    return True
+pass
+
+
+def get_vllm_state_dict(llm, return_state_dict = False):
+    # All Unsloth Zoo code licensed under LGPLv3
+    # Unmerges vLLM modules and returns HF equivalent state_dict
+    try:
+        vllm_internals = llm.llm_engine.model_executor.driver_worker.model_runner.model
+    except:
+        raise RuntimeError("Unsloth: Failed to access llm.llm_engine.model_executor.driver_worker.model_runner.model")
+    pass
+    state_dict = OrderedDict()
+    quant_state_dict = OrderedDict()
+
+    def get_state_dict(prefix, kk, state_dict, proj):
+        qweight = proj.weight
+        if hasattr(proj, "output_sizes"):
+            dim_offsets = np.cumsum([0] + proj.output_sizes)
+        else:
+            dim_offsets = [0, qweight.shape[0]]
+        pass
+
+        if hasattr(qweight, "bnb_quant_state"):
+            # Bitsandbytes quantizations
+            quant_states = qweight.bnb_quant_state
+            offsets = qweight.bnb_shard_offsets
+            state_dict[prefix + ".weight"] = qweight[offsets[kk] : offsets[kk + 1]]
+            quant_state_dict[prefix + ".weight.quant_state"] = quant_states[kk]
+            quant_state_dict[prefix + ".weight"] = state_dict[prefix + ".weight"]
+            quant_state = quant_states[kk].as_dict(packed = True)
+            for k, v in quant_state.items():
+                state_dict[prefix + ".weight." + k] = v
+            pass
+        else:
+            # Normal FP16 weights
+            qweight.requires_grad_(False) # Disable grad - sometimes vLLM forgets
+            state_dict[prefix + ".weight"] = qweight[dim_offsets[kk] : dim_offsets[kk + 1]]
+            quant_state_dict[prefix + ".weight"] = state_dict[prefix + ".weight"]
+        pass
+
+        # Check bias
+        bias = getattr(proj, "bias", None)
+        if bias is not None:
+            bias.requires_grad_(False) # Disable grad - sometimes vLLM forgets
+            state_dict[prefix + ".bias"] = bias[dim_offsets[kk] : dim_offsets[kk + 1]]
+            quant_state_dict[prefix + ".bias"] = state_dict[prefix + ".bias"]
+        pass
+    pass
+
+    state_dict["model.embed_tokens.weight"] = vllm_internals.model.embed_tokens.weight.data
+    quant_state_dict["model.embed_tokens.weight"] = state_dict["model.embed_tokens.weight"]
+    for kk in range(len(vllm_internals.model.layers)):
+        proj = vllm_internals.model.layers[kk].self_attn.qkv_proj
+        get_state_dict(f"model.layers.{kk}.self_attn.q_proj", 0, state_dict, proj)
+        get_state_dict(f"model.layers.{kk}.self_attn.k_proj", 1, state_dict, proj)
+        get_state_dict(f"model.layers.{kk}.self_attn.v_proj", 2, state_dict, proj)
+
+        proj = vllm_internals.model.layers[kk].self_attn.o_proj
+        get_state_dict(f"model.layers.{kk}.self_attn.o_proj", 0, state_dict, proj)
+
+        proj = vllm_internals.model.layers[kk].mlp.gate_up_proj
+        get_state_dict(f"model.layers.{kk}.mlp.gate_proj", 0, state_dict, proj)
+        get_state_dict(f"model.layers.{kk}.mlp.up_proj",   1, state_dict, proj)
+
+        proj = vllm_internals.model.layers[kk].mlp.down_proj
+        get_state_dict(f"model.layers.{kk}.mlp.down_proj", 0, state_dict, proj)
+
+        state_dict[f"model.layers.{kk}.input_layernorm.weight"] = \
+            vllm_internals.model.layers[kk].input_layernorm.state_dict()["weight"]
+        quant_state_dict[f"model.layers.{kk}.input_layernorm.weight"] = \
+            state_dict[f"model.layers.{kk}.input_layernorm.weight"]
+
+        state_dict[f"model.layers.{kk}.post_attention_layernorm.weight"] = \
+            vllm_internals.model.layers[kk].post_attention_layernorm.state_dict()["weight"]
+        quant_state_dict[f"model.layers.{kk}.post_attention_layernorm.weight"] = \
+            state_dict[f"model.layers.{kk}.post_attention_layernorm.weight"]
+    pass
+
+    state_dict["model.norm.weight"] = vllm_internals.model.norm.weight.data
+    quant_state_dict["model.norm.weight"] = state_dict["model.norm.weight"]
+
+    if getattr(config, "tie_word_embeddings", True) is False:
+        state_dict["lm_head.weight"] = vllm_internals.lm_head.weight.data
+        quant_state_dict["lm_head.weight"] = state_dict["lm_head.weight"]
+    pass
+
+    if not return_state_dict: state_dict = None
+    return state_dict, quant_state_dict
+pass
+
+
+def assert_same_state_dict(old_state_dict, new_state_dict):
+    # All Unsloth Zoo code licensed under LGPLv3
+    # Check if state_dict are equivalent
+
+    difference = new_state_dict.keys() ^ old_state_dict.keys()
+    difference -= set(("lm_head.weight",))
+    if len(difference) != 0:
+        raise RuntimeError(f"Unsloth: Failed comparing state_dict with {difference}")
+    pass
+
+    for key in old_state_dict:
+        try:
+            torch.testing.assert_close(state_dict[key], old_state_dict[key], check_stride = True)
+        except Exception as error:
+            if key == "lm_head.weight":
+                # Maybe tied embeddings?
+                key1 = key if key in state_dict else "model.embed_tokens.weight"
+                key2 = key if key in old_state_dict else "model.embed_tokens.weight"
+                torch.testing.assert_close(state_dict[key1], old_state_dict[key2], check_stride = True)
+            else:
+                raise RuntimeError(f"[{key}]\n{str(error)}")
+        pass
+    pass
+pass
+
+
+def create_empty_causal_lm(config, dtype = torch.float16):
+    # All Unsloth Zoo code licensed under LGPLv3
+    # Empty model from config
+    new_config = deepcopy(config)
+    new_config.intermediate_size = 0
+    new_config.hidden_size = 0
+    new_config.vocab_size = 1
+    new_config.pad_token_id = 0
+    new_model = AutoModelForCausalLM.from_config(
+        new_config,
+        attn_implementation = "eager",
+    )
+    new_model = new_model.to(device = "cuda:0", dtype = dtype)
+    return new_model
+pass
+
+
+def convert_vllm_to_huggingface(quant_state_dict, config, dtype = torch.float16):
+    # All Unsloth Zoo code licensed under LGPLv3
+    # Unmerges vLLM modules to create HF compatible model
+    new_model = create_empty_causal_lm(config, dtype)
+    quantization_config = config.quantization_config
+    kwargs = dict()
+    # Get quantization_config flags
+    compute_dtype = _get_dtype(quantization_config["bnb_4bit_compute_dtype"])
+    kwargs["compress_statistics"] = quantization_config["bnb_4bit_use_double_quant"]
+    kwargs["quant_type"] = quantization_config["bnb_4bit_quant_type"]
+    kwargs["quant_storage"] = _get_dtype(quantization_config["bnb_4bit_quant_storage"])
+
+    from bitsandbytes.nn.modules import Linear4bit, Params4bit
+    from torch.nn.modules import Linear
+
+    layer_names = [
+        "model.layers.{kk}.self_attn.q_proj",
+        "model.layers.{kk}.self_attn.k_proj",
+        "model.layers.{kk}.self_attn.v_proj",
+        "model.layers.{kk}.self_attn.o_proj",
+        "model.layers.{kk}.mlp.gate_proj",
+        "model.layers.{kk}.mlp.up_proj",
+        "model.layers.{kk}.mlp.down_proj",
+        "model.layers.{kk}.input_layernorm",
+        "model.layers.{kk}.post_attention_layernorm",
+    ]
+    layernorm_names = [
+        "input_layernorm",
+        "post_attention_layernorm",
+    ]
+
+    for kk in range(config.num_hidden_layers):
+        for layer_name in layer_names:
+            layer_name = layer_name.format(kk = kk)
+            weight = quant_state_dict[f"{layer_name}.weight"]
+
+            if f"{layer_name}.weight.bias" in quant_state_dict:
+                # Has bias!
+                has_bias = True
+                bias = quant_state_dict[f"{layer_name}.weight.bias"]
+            else:
+                has_bias = False
+                bias = None
+            pass
+
+            if f"{layer_name}.weight.quant_state" in quant_state_dict:
+                # Layer is quantized!
+                quant_state = quant_state_dict[f"{layer_name}.weight.quant_state"]
+                n_layers = config.num_hidden_layers
+                layer = Linear4bit(0, 0, device = "cuda:0", bias = has_bias, compute_dtype = compute_dtype, **kwargs)
+                layer.in_features  = quant_state.shape[1]
+                layer.out_features = quant_state.shape[0]
+                layer.weight = Params4bit(data = weight, requires_grad = False, **kwargs)
+                layer.weight.quant_state = quant_state
+                layer.bias = bias
+            elif not any(x in layer_name for x in layernorm_names):
+                layer = Linear(0, 0, device = "cuda:0", bias = has_bias)
+                layer.in_features  = weight.shape[1]
+                layer.out_features = weight.shape[0]
+                layer.weight = torch.nn.Parameter(weight, requires_grad = False)
+                layer.bias = bias
+            else:
+                # Layernorms
+                weight = torch.nn.Parameter(weight, requires_grad = False)
+                layer_name = re.sub(r"\.([\d]{1,})\.", r"[\1].", layer_name)
+                exec(f"new_model.{layer_name}.weight = weight")
+                continue
+            pass
+            
+            # Convert model.layers.0.self_attn.q_proj to model.layers[0].self_attn.q_proj
+            layer_name = re.sub(r"\.([\d]{1,})\.", r"[\1].", layer_name)
+            exec(f"new_model.{layer_name} = layer")
+        pass
+    pass
+
+    # Norm
+    norm = quant_state_dict["model.norm.weight"]
+    norm = torch.nn.Parameter(norm, requires_grad = False)
+    new_model.model.norm.weight = norm
+
+    # Embeddings
+    new_model.model.embed_tokens = torch.nn.Embedding.from_pretrained(
+        quant_state_dict["model.embed_tokens.weight"],
+        freeze = True,
+        padding_idx = config.pad_token_id,
+    )
+
+    # LM Head
+    if getattr(config, "tie_word_embeddings", False):
+        weight = quant_state_dict["model.embed_tokens.weight"]
+    else:
+        weight = quant_state_dict["lm_head.weight"]
+    layer = Linear(0, 0, device = "cuda:0", bias = has_bias)
+    layer.in_features  = weight.shape[1]
+    layer.out_features = weight.shape[0]
+    layer.weight = torch.nn.Parameter(weight, requires_grad = False)
+    new_model.lm_head = layer
+    if getattr(config, "tie_word_embeddings", False): new_model.tie_weights()
+
+    # Fix up config file
+    for module in new_model.modules():
+        if hasattr(module, "config"):
+            module.config = config
+        if hasattr(module, "intermediate_size"):
+            module.intermediate_size = config.intermediate_size
+        if hasattr(module, "hidden_size"):
+            module.hidden_size = config.hidden_size
+    pass
+    new_model.config = config
+
+    # Cleanup
+    import gc
+    for _ in range(3):
+        gc.collect()
+        torch.cuda.empty_cache()
+    return new_model
+pass
+
+
+def test_get_vllm_state_dict(
+    model_name = "unsloth/Llama-3.2-3B-Instruct-unsloth-bnb-4bit",
+    dtype = torch.float16,
+):
+    # All Unsloth Zoo code licensed under LGPLv3
+    # Check if model is allowed to be used in vLLM
+    from transformers import AutoConfig
+    config = AutoConfig.from_pretrained(
+        model_name,
+        token = None,
+        revision = None,
+        trust_remote_code = False,
+    )
+    if not vllm_dynamic_quant_supported(model_name, config):
+        raise NotImplementedError(f"Unsloth: Dynamic quant of {model_name} not supported in vLLM")
+
+    from unsloth import FastLanguageModel
+    model, tokenizer = FastLanguageModel.from_pretrained(
+        model_name = model_name,
+        max_seq_length = 2048,
+        dtype = None,
+        load_in_4bit = True,
+        use_exact_model_name = True,
+    )
+
+    from vllm import LLM
+    llm = LLM(
+        model = model_name,
+        gpu_memory_utilization = 0.5,
+        max_model_len = 8192,
+        quantization = "bitsandbytes",
+        load_format = "bitsandbytes",
+    )
+
+    state_dict, quant_state_dict = get_vllm_state_dict(llm, return_state_dict = True)
+    assert_same_state_dict(model.state_dict(), state_dict)
+
+    new_model = convert_vllm_to_huggingface(quant_state_dict, config, dtype)
+    assert_same_state_dict(model.state_dict(), new_model.state_dict())
+pass
 
 # Unsloth Zoo - Utilities for Unsloth
 # Copyright 2023-present Daniel Han-Chen & the Unsloth team. All rights reserved.
