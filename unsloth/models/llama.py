18de8f29c8b7130a6d7adebebb5d520f2af8c0df
Daniel Han <danielhanchen@gmail.com>
2025-02-05 20:00:02 -0800

    Fp8 cache

--- unsloth/models/llama.py
+++ unsloth/models/llama.py
@@ -1637,7 +1637,7 @@ class FastLlamaModel:
 
         fast_inference    = False, # uses vLLM
         gpu_memory_utilization = 0.5,
-        float8_kv_cache   = True,
+        float8_kv_cache   = False,
         random_state      = 3407,
         max_lora_rank     = 16,
         disable_log_stats = False,
