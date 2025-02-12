f121a5c37dc5f087c925944b9ee798d13f288eaa
Daniel Han <danielhanchen@gmail.com>
2025-02-12 03:03:43 -0800

    Update llama.py

--- unsloth/models/llama.py
+++ unsloth/models/llama.py
@@ -1154,6 +1154,7 @@ def CausalLM_fast_forward(fast_forward_inference):
             output = (logits,) + outputs[1:]
             return (loss,) + output if loss is not None else output
 
+        print("========== dtype = ", logits.dtype)
         return CausalLMOutputWithPast(
             loss=loss,
             logits=logits,
