8de588b4df1091d3da0d635b01e1417b24c4eda7
Daniel Han <danielhanchen@gmail.com>
2025-02-12 21:06:33 -0800

    Update llama.py

--- unsloth/models/llama.py
+++ unsloth/models/llama.py
@@ -707,8 +707,8 @@ def LlamaModel_fast_forward(
     # Ignore attention_mask
     if attention_mask is None:
         padding_mask = None
-    elif attention_mask is None and self.training:
-        # attention_mask = None
+    elif self.training:
+        attention_mask = None
         padding_mask = None
     else:
         # if 0 in attention_mask:
