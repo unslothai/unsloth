46ce30f0b241f408f5f93763c7e04282b82d49fd
Daniel Han <danielhanchen@gmail.com>
2025-02-20 02:38:26 -0800

    Update rl_replacements.py

--- unsloth_zoo/rl_replacements.py
+++ unsloth_zoo/rl_replacements.py
@@ -61,7 +61,9 @@ def grpo_compute_loss(old_logits, new_logits, input_ids, mask, beta, advantages)
     new = new_x - torch.logsumexp(new_logits, dim = -1)
 
     kl_i = torch.exp(old - new) - (old - new) - 1.0
-    kl_i = torch.exp(new) * kl_i
+    kl_i = torch.exp(old) * (old - new)
+    # Full correct reverse KL divergence?? Missing term maybe?
+    # kl_i = torch.exp(new) * kl_i
     # Must detach - otherwise gradients are not propagated correctly!
     # exp(x - x) == 1
     loss_i = torch.exp(new - new.detach()) * advantages.unsqueeze(1)
