2c438b3b99db8bf606ef9b28686bf77f4a83f95b
Daniel Han <danielhanchen@gmail.com>
2025-02-20 07:01:14 -0800

    Update llama.py

--- unsloth/models/llama.py
+++ unsloth/models/llama.py
@@ -449,6 +449,7 @@ def LlamaAttention_fast_forward(
     else:
         # Grouped query attention
         if SDPA_HAS_GQA:
+            print(attention_mask)
             # Needs (batch_size, n_heads, seq_len, head_dim)
             # is_casual and attention_mask must not be both set!
             A = scaled_dot_product_attention(Q, K, V, attn_mask = attention_mask, is_causal = False, enable_gqa = n_groups != 1)
