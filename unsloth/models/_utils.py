beb48e1edc83012faefe20614bf4db14ab970d1e
Daniel Han <danielhanchen@gmail.com>
2025-02-20 04:22:17 -0800

    Versioning

--- unsloth/models/_utils.py
+++ unsloth/models/_utils.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "2025.2.12"
+__version__ = "2025.2.13"
 
 __all__ = [
     "SUPPORTS_BFLOAT16",
