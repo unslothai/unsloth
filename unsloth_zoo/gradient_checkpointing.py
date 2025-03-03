7812e4177c184e4c58b9a40b5ea358106ca0ce9e
Daniel Han <danielhanchen@gmail.com>
2025-03-03 02:30:46 -0800

    Update gradient_checkpointing.py

--- unsloth_zoo/gradient_checkpointing.py
+++ unsloth_zoo/gradient_checkpointing.py
@@ -142,6 +142,7 @@ class Unsloth_Offloaded_Gradient_Checkpointer(torch.autograd.Function):
     @staticmethod
     @torch_amp_custom_fwd
     def forward(ctx, forward_function, hidden_states, *args):
+        ctx.device = hidden_states.device
         saved_hidden_states = hidden_states.to("cpu", non_blocking = True)
         with torch.no_grad():
             output = forward_function(hidden_states, *args)
@@ -155,7 +156,7 @@ class Unsloth_Offloaded_Gradient_Checkpointer(torch.autograd.Function):
     @torch_amp_custom_bwd
     def backward(ctx, dY):
         (hidden_states,) = ctx.saved_tensors
-        hidden_states = hidden_states.to("cuda:0", non_blocking = True).detach()
+        hidden_states = hidden_states.to(ctx.device, non_blocking = True).detach()
         hidden_states.requires_grad_(True)
         with torch.enable_grad():
             (output,) = ctx.forward_function(hidden_states, *ctx.args)
@@ -481,8 +482,6 @@ class UnslothCheckpointFunction(torch.autograd.Function):
             global EXTRA_STREAM
             buffer = GPU_BUFFER[:new_size].view(shape)
             x = CPU_BUFFERS[CPU_INDEX][:new_size].view(shape)
-            if buffer.device != torch.cuda.current_device():
-                print("#########", buffer.device, torch.cuda.current_device())
 
             # See https://pytorch.org/docs/stable/notes/cuda.html#cuda-streams
             EXTRA_STREAM.wait_stream(MAIN_STREAM)
