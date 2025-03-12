08c4a4f2a933b663c4e26c50cf654f5486c5417b
Daniel Han <danielhanchen@gmail.com>
2025-03-11 22:11:58 -0700

    Update loss_utils.py

--- unsloth_zoo/loss_utils.py
+++ unsloth_zoo/loss_utils.py
@@ -276,7 +276,6 @@ def _unsloth_get_batch_samples(self, epoch_iterator, num_batches):
         except Exception as exception:
             logger.warning_once(exception)
     pass
-    print("num_items_in_batch", num_items_in_batch)
     return batch_samples, num_items_in_batch
 pass
 
