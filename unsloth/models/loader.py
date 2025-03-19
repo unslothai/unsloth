67d169a927454c9ff881e6b6fb234a2bc26ce069
Daniel Han <danielhanchen@gmail.com>
2025-03-19 08:41:40 -0700

    Update loader.py

--- unsloth/models/loader.py
+++ unsloth/models/loader.py
@@ -646,10 +646,11 @@ class FastModel(FastBaseModel):
         # Set forced float32 env flag
         os.environ["UNSLOTH_FORCE_FLOAT32"] = "0"
         do_forced_float32 = False
-        model_type_arch = model_types[1]
+        for model_type_arch in model_types:
+            if model_type_arch != "siglip": break
         global FORCE_FLOAT32
         for disable_name in FORCE_FLOAT32:
-            print(disable_name.lower(), model_type_arch.lower(), model_name.lower(), dtype, SUPPORTS_BFLOAT16)
+            print(model_types, disable_name.lower(), model_type_arch.lower(), model_name.lower(), dtype, SUPPORTS_BFLOAT16)
             if (disable_name.lower() == model_type_arch.lower() or \
                 disable_name.lower() in model_name.lower()) and \
                 ((dtype == torch.float16) or not SUPPORTS_BFLOAT16):
