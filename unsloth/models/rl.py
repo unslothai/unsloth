5b73657beb13458ead2cecf9183ddd19c5eeec2e
pluesclues <136766175+pluesclues@users.noreply.github.com>
2025-03-23 00:16:42 -0400

    Update rl.py, disabling adapters screws inference up

--- unsloth/models/rl.py
+++ unsloth/models/rl.py
@@ -899,7 +899,7 @@ def _patch_trl_rl_trainers(trainer_file = "grpo_trainer"):
     # Get final source code
     # if RLTrainer_name == "RLOOTrainer" or RLTrainer_name == "OnlineDPOTrainer":
     #     breakpoint()
-    if RLTrainer_name == "RLOOTrainer" or RLTrainer_name == "PPOTrainer":
+    if RLTrainer_name == "RLOOTrainer":
         #breakpoint()
         #selective_log_softmax_code = "from trl.trainer.utils import selective_log_softmax"
         RLTrainer_source = RLTrainer_replacement_rloo_ppo.format(
@@ -971,11 +971,12 @@ def _patch_trl_rl_trainers(trainer_file = "grpo_trainer"):
         new_eval_loop = r"""query_response, _ = batch_generation(unwrapped_model, query,"""
 
         RLTrainer_source = re.sub(old_eval_loop, new_eval_loop, RLTrainer_source)
-        replacement = "with model.disable_adapter():\n    ref_output = forward(model, query_response, processing_class.pad_token_id)"
-        code = """#self.ref_policy = self.ref_policy.to(self.accelerator.device)"""
-        RLTrainer_source = re.sub(r"ref_output = forward\(ref_policy, query_response, processing_class\.pad_token_id\)", replacement, RLTrainer_source)
-        RLTrainer_source = re.sub(r"ref_output = forward\(ref_policy, query_response, processing_class\.pad_token_id\)", replacement, RLTrainer_source)
-        RLTrainer_source = re.sub(r"self\.ref_policy = self\.ref_policy\.to\(self.accelerator\.device\)", code, RLTrainer_source)
+        # replacement = r"""with torch.no_grad():
+        #                 with model.disable_adapter(): ref_output = forward(model, query_response, processing_class.pad_token_id)"""        
+        # code = """#self.ref_policy = self.ref_policy.to(self.accelerator.device)"""
+        # RLTrainer_source = re.sub(r"ref_output = forward\(ref_policy, query_response, processing_class\.pad_token_id\)", replacement, RLTrainer_source)
+        # #RLTrainer_source = re.sub(r"ref_output = forward\(ref_policy, query_response, processing_class\.pad_token_id\)", replacement, RLTrainer_source)
+        # RLTrainer_source = re.sub(r"self\.ref_policy = self\.ref_policy\.to\(self.accelerator\.device\)", code, RLTrainer_source)
 
         peft_replace = """unwrapped_model,"""
 
