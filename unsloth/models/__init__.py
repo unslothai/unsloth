53d820d31e5ac38c52c4a994c47e3194727495fe
datta0 <venkatadattasainimmaturi@gmail.com>
2025-03-28 05:58:01 +0000

    Enable qwen3 and qwen3moe

--- unsloth/models/__init__.py
+++ unsloth/models/__init__.py
@@ -12,11 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .llama   import FastLlamaModel
-from .loader  import FastLanguageModel, FastVisionModel, FastTextModel, FastModel
-from .mistral import FastMistralModel
-from .qwen2   import FastQwen2Model
-from .granite import FastGraniteModel
-from .dpo     import PatchDPOTrainer, PatchKTOTrainer
-from ._utils  import is_bfloat16_supported, __version__
-from .rl      import PatchFastRL, vLLMSamplingParams
+from .llama     import FastLlamaModel
+from .loader    import FastLanguageModel, FastVisionModel, FastTextModel, FastModel
+from .mistral   import FastMistralModel
+from .qwen2     import FastQwen2Model
+from .qwen3     import FastQwen3Model
+from .qwen3_moe import FastQwen3MoeModel
+from .granite   import FastGraniteModel
+from .dpo       import PatchDPOTrainer, PatchKTOTrainer
+from ._utils    import is_bfloat16_supported, __version__
+from .rl        import PatchFastRL, vLLMSamplingParams
