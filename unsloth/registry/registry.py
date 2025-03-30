6747831d318d9f4c9b785e9edb76ce54ecdb1be1
jeromeku <jerome.ku@gmail.com>
2025-03-30 11:36:48 -0700

    start registry reog

---
+++ unsloth/registry/registry.py
@@ -0,0 +1,149 @@
+from dataclasses import dataclass, field
+from typing import Literal
+
+BNB_QUANTIZED_TAG = "bnb-4bit"
+UNSLOTH_DYNAMIC_QUANT_TAG = "unsloth" + "-" + BNB_QUANTIZED_TAG
+QUANT_TYPE_MAP = {
+    "bnb": BNB_QUANTIZED_TAG,
+    "unsloth": UNSLOTH_DYNAMIC_QUANT_TAG,
+    "GGUF": "GGUF",
+}
+QUANT_TYPES = list(QUANT_TYPE_MAP.keys())
+
+
+@dataclass
+class ModelInfo:
+    org: str
+    base_name: str
+    version: str
+    size: int
+    name: str = None  # full model name, constructed from base_name, version, and size unless provided
+    is_multimodal: bool = False
+    instruct_tag: str = None
+    quant_type: Literal["bnb", "unsloth"] = None
+
+    def __post_init__(self):
+        self.name = self.name or self.construct_model_name(
+            self.base_name,
+            self.version,
+            self.size,
+            self.quant_type,
+            self.instruct_tag,
+        )
+
+    @staticmethod
+    def append_instruct_tag(key: str, instruct_tag: str = None):
+        if instruct_tag:
+            key = "-".join([key, instruct_tag])
+        return key
+
+    @staticmethod
+    def append_quant_type(
+        key: str, quant_type: Literal["bnb", "unsloth", "GGUF"] = None
+    ):
+        if quant_type:
+            if quant_type == "bnb":
+                key = "-".join([key, QUANT_TYPE_MAP["bnb"]])
+            elif quant_type == "unsloth":
+                key = "-".join([key, QUANT_TYPE_MAP["unsloth"]])
+            elif quant_type == "GGUF":
+                key = "-".join([key, QUANT_TYPE_MAP["GGUF"]])
+        return key
+
+    @classmethod
+    def construct_model_name(cls, base_name, version, size, quant_type, instruct_tag):
+        raise NotImplementedError("Subclass must implement this method")
+
+    @property
+    def model_path(
+        self,
+    ) -> str:
+        return f"{self.org}/{self.name}"
+
+
+@dataclass
+class ModelMeta:
+    org: str
+    base_name: str
+    model_version: str
+    model_info_cls: type[ModelInfo]
+    model_sizes: list[str] = field(default_factory=list)
+    instruct_tags: list[str] = field(default_factory=list)
+    quant_types: list[Literal[None, "bnb", "unsloth"]] = field(default_factory=list)
+    is_multimodal: bool = False
+
+
+MODEL_REGISTRY: dict[str, ModelInfo] = {}
+
+
+def register_model(
+    model_info_cls: ModelInfo,
+    org: str,
+    base_name: str,
+    version: str,
+    size: int,
+    instruct_tag: str = None,
+    quant_type: Literal["bnb", "unsloth"] = None,
+    is_multimodal: bool = False,
+    name: str = None,
+):
+    name = name or model_info_cls.construct_model_name(
+        base_name=base_name,
+        version=version,
+        size=size,
+        quant_type=quant_type,
+        instruct_tag=instruct_tag,
+    )
+    key = f"{org}/{name}"
+
+    if key in MODEL_REGISTRY:
+        raise ValueError(f"Model {key} already registered")
+
+    MODEL_REGISTRY[key] = model_info_cls(
+        org=org,
+        base_name=base_name,
+        version=version,
+        size=size,
+        is_multimodal=is_multimodal,
+        instruct_tag=instruct_tag,
+        quant_type=quant_type,
+        name=name,
+    )
+
+def _check_model_info(model_id: str, properties: list[str] = ["lastModified"]):
+    from huggingface_hub import HfApi
+    from huggingface_hub import ModelInfo as HfModelInfo
+    api = HfApi()
+
+    try:
+        model_info: HfModelInfo = api.model_info(model_id, expand=properties)
+    except Exception as e:
+        print(f"Error getting model info for {model_id}: {e}")
+        model_info = None
+    return model_info
+
+
+def _register_models(model_meta: ModelMeta):
+    org = model_meta.org
+    base_name = model_meta.base_name
+    instruct_tags = model_meta.instruct_tags
+    model_version = model_meta.model_version
+    model_sizes = model_meta.model_sizes
+    is_multimodal = model_meta.is_multimodal
+    quant_types = model_meta.quant_types
+    model_info_cls = model_meta.model_info_cls
+
+    for size in model_sizes:
+        for instruct_tag in instruct_tags:
+            for quant_type in quant_types:
+                _org = "unsloth" if quant_type is not None else org
+                register_model(
+                    model_info_cls=model_info_cls,
+                    org=_org,
+                    base_name=base_name,
+                    version=model_version,
+                    size=size,
+                    instruct_tag=instruct_tag,
+                    quant_type=quant_type,
+                    is_multimodal=is_multimodal,
+                )
