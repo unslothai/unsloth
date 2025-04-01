e61106ad05cf73407be0ca291fbd3aaa5d72bcaf
jeromeku <jerome.ku@gmail.com>
2025-03-31 17:01:51 -0700

    rename model registration methods

---
+++ unsloth/registry/__init__.py
@@ -0,0 +1,5 @@
+# from ._deepseek import register_deepseek_models, register
+# from ._llama import register_llama_models, register_llama_vision_models
+# from ._mistral import register_mistral_models
+# from ._openai import register_openai_models
+# from ._qwen import register_qwen_models
