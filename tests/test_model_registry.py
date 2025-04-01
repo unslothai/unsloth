7dbf51ab2c77bf815af795fcd57f80b23c823bc7
jeromeku <jerome.ku@gmail.com>
2025-03-31 17:36:19 -0700

    add model search method

--- tests/test_model_registry.py
+++ tests/test_model_registry.py
@@ -1,3 +1,13 @@
+"""
+
+Test model registration methods
+Checks that model registration methods work for respective models as well as all models
+The check is performed
+- by registering the models
+- checking that the instantiated models can be found on huggingface hub by querying for the model id
+
+"""
+
 from dataclasses import dataclass
 
 import pytest
@@ -10,7 +20,7 @@ from unsloth.registry._llama import register_llama_models
 from unsloth.registry._mistral import register_mistral_models
 from unsloth.registry._phi import register_phi_models
 from unsloth.registry._qwen import register_qwen_models
-from unsloth.registry.registry import MODEL_REGISTRY, ModelInfo
+from unsloth.registry.registry import MODEL_REGISTRY
 from unsloth.utils.hf_hub import get_model_info
 
 MODEL_NAMES = [
@@ -34,7 +44,7 @@ MODEL_REGISTRATION_METHODS = [
 @dataclass
 class ModelTestParam:
     name: str
-    registration_models: callable
+    register_models: callable
 
 
 def _test_model_uploaded(model_ids: list[str]):
@@ -52,13 +62,13 @@ TestParams = [
     for name, models in zip(MODEL_NAMES, MODEL_REGISTRATION_METHODS)
 ]
 
+
 # Test that model registration methods register respective models
-@pytest.mark.parametrize(
-    "model_test_param", TestParams, ids=lambda param: param.name
-)
+@pytest.mark.parametrize("model_test_param", TestParams, ids=lambda param: param.name)
 def test_model_registration(model_test_param: ModelTestParam):
     MODEL_REGISTRY.clear()
-    model_test_param.registration_models()
+    registration_method = model_test_param.register_models
+    registration_method()
     registered_models = MODEL_REGISTRY.keys()
     missing_models = _test_model_uploaded(registered_models)
     assert not missing_models, (
@@ -66,23 +76,8 @@ def test_model_registration(model_test_param: ModelTestParam):
     )
 
 
-# if __name__ == "__main__":
-#     for method in [
-#         get_llama_models,
-#         get_llama_vision_models,
-#         get_qwen_models,
-#         get_qwen_vl_models,
-#         get_phi_models,
-#         get_phi_instruct_models,
-#     ]:
-#         models = method()
-#         model_name = next(iter(models.values())).base_name
-#         print(f"{model_name}: {len(models)} registered")
-#         for model_info in models.values():
-#             print(f"  {model_info.model_path}")
-#         missing_models = test_model_uploaded(list(models.keys()))
-
-#         if missing_models:
-#             print("--------------------------------")
-#             print(f"Missing models: {missing_models}")
-#             print("--------------------------------")
+def test_all_model_registration():
+    register_models()
+    registered_models = MODEL_REGISTRY.keys()
+    missing_models = _test_model_uploaded(registered_models)
+    assert not missing_models, f"Missing following models: {missing_models}"
