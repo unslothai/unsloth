2ff490e23b2015a42a2e50be3f1f6fdc485b8332
jeromeku <jerome.ku@gmail.com>
2025-03-31 17:11:35 -0700

    add global register models

--- unsloth/registry/__init__.py
+++ unsloth/registry/__init__.py
@@ -1,5 +1,13 @@
-# from ._deepseek import register_deepseek_models, register
-# from ._llama import register_llama_models, register_llama_vision_models
-# from ._mistral import register_mistral_models
-# from ._openai import register_openai_models
-# from ._qwen import register_qwen_models
+from ._deepseek import register_deepseek_models as _register_deepseek_models
+from ._gemma import register_gemma_models as _register_gemma_models
+from ._llama import register_llama_models as _register_llama_models
+from ._mistral import register_mistral_models as _register_mistral_models
+from ._phi import register_phi_models as _register_phi_models
+from ._qwen import register_qwen_models as _register_qwen_models
+
+_register_deepseek_models()
+_register_gemma_models()
+_register_llama_models()
+_register_mistral_models()
+_register_phi_models()
+_register_qwen_models()
