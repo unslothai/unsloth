152cde67663d7ea33b94039f61f5ce040eddda31
Daniel Han <danielhanchen@gmail.com>
2025-05-01 06:56:02 -0700

    Update synthetic.py

--- unsloth/dataprep/synthetic.py
+++ unsloth/dataprep/synthetic.py
@@ -206,7 +206,7 @@ class SyntheticDataKit:
 
         with open(filename, "r") as f: text = f.read()
 
-        max_tokens = self.max_seq_length - self.max_generation_tokens*3 # * 3 to reduce errors
+        max_tokens = self.max_seq_length - self.max_generation_tokens*2 - 128 # -128 to reduce errors
         if max_tokens <= 5:
             raise RuntimeError("Generation length is way too long!")
         input_ids = self.tokenizer(text, add_special_tokens = False).input_ids
