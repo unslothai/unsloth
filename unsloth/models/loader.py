636aa9b97755df37e648eae3032f81238f7ce354
Daniel Han <danielhanchen@gmail.com>
2025-05-15 02:32:29 -0700

    is_multimodal

--- unsloth/models/loader.py
+++ unsloth/models/loader.py
@@ -485,6 +485,7 @@ class FastModel(FastBaseModel):
         auto_model                 = None,
         whisper_language           = None,
         whisper_task               = None,
+        is_multimodal              = None,
         *args, **kwargs,
     ):
         if token is None: token = get_token()
@@ -541,8 +542,8 @@ class FastModel(FastBaseModel):
             if transformers_version < Version("4.50.0.dev0"):
                 raise RuntimeError("Unsloth: Granite Vision only works on transformers >= 4.50.0." + NIGHTLY)
         elif "csm-1b" in model_name.lower():
-            os.environ["UNSLOTH_COMPILE_DISABLE"] = "1"
-            os.environ["UNSLOTH_DISABLE_FAST_GENERATION"] = "0"
+            os.environ["UNSLOTH_COMPILE_DISABLE"] = "0"
+            os.environ["UNSLOTH_DISABLE_FAST_GENERATION"] = "1"
             os.environ["UNSLOTH_FORCE_CUSTOM_DTYPE"] = "torch.float16;if name.endswith(('_proj', 'fc1', 'fc2', 'codebook', 'head')): module.to(torch.float16)"
         elif "olmo-2" in model_name.lower() and transformers_version < Version("4.50.0.dev0"):
             raise RuntimeError("Unsloth: OLMo-2 only works on transformers >= 4.50.0." + NIGHTLY)
