7aaab18515a3c6976630adc30f5045f8ae0f30b3
simpissa <matthewzhao420@gmail.com>
2025-06-08 00:49:54 -0500

    slice last logit

--- unsloth/models/rl_replacements.py
+++ unsloth/models/rl_replacements.py
@@ -223,6 +223,7 @@ def grpo_trainer__get_per_token_logps(function_name, function):
             # We add 1 to `logits_to_keep` because the last logits of the sequence is later excluded
             hidden_states = model(input_ids=input_ids, attention_mask=attention_mask, logits_to_keep=logits_to_keep + 1).logits
             #logits = logits[:, :-1, :]  # (B, L-1, V), exclude the last logit: it corresponds to the next token pred
+            hidden_states = hidden_states[:, :-1, :]
             return hidden_states
             # input_ids = input_ids[:, -logits_to_keep:]
             # For transformers<=4.48, logits_to_keep argument isn't supported, so here we drop logits ourselves.
