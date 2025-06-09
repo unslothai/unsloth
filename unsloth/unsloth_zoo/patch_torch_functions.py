145d1c7dedfc94119aaa130c90d6c8562958401d
Mathew Mathew <mmathew23@gmail.com>
2025-06-08 22:12:13 -0500

    license edits

--- unsloth/unsloth_zoo/patch_torch_functions.py
+++ unsloth/unsloth_zoo/patch_torch_functions.py
@@ -1,4 +1,3 @@
-
 # Unsloth Zoo - Utilities for Unsloth
 # Copyright 2023-present Daniel Han-Chen, Michael Han-Chen & the Unsloth team. All rights reserved.
 #
--- unsloth/unsloth_zoo/vllm_lora_worker_manager.py
+++ unsloth/unsloth_zoo/vllm_lora_worker_manager.py
@@ -1,3 +1,19 @@
+# Unsloth Zoo - Utilities for Unsloth
+# Copyright 2023-present Daniel Han-Chen, Michael Han-Chen & the Unsloth team. All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # From https://github.com/vllm-project/vllm/pull/12609
 from contextlib import contextmanager
 from typing import Any, Dict, List, Literal, Optional, Set, Type, Union
