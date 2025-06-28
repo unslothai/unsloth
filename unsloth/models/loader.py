6e72d688f564c7ad8c804b1dfb97327e28cefd84
DoubleMathew <mmathew23@gmail.com>
2025-06-28 07:27:56 -0500

    granite force layernorm upcast (#2799)

--- unsloth/models/loader.py
+++ unsloth/models/loader.py
@@ -556,6 +556,10 @@ class FastModel(FastBaseModel):
         elif "csm-1b" in lowered_model_name:
             os.environ["UNSLOTH_DISABLE_STATIC_GENERATION"] = "1" # Sesame fails
             os.environ["UNSLOTH_FORCE_CUSTOM_DTYPE"] = "torch.float16;if name.endswith(('_proj', 'fc1', 'fc2', 'codebook', 'head')): module.to(torch.float16)"
+        elif 'granite-4' in lowered_model_name:
+            # granite-4 rms norms are stored as 16 bit, but we upcast
+            os.environ["UNSLOTH_UPCAST_LAYERNORM"] = "1"
+            os.environ["UNSLOTH_DISABLE_STATIC_GENERATION"] = "1"
         elif "olmo-2" in lowered_model_name and transformers_version < Version("4.50.0.dev0"):
             raise RuntimeError("Unsloth: OLMo-2 only works on transformers >= 4.50.0." + NIGHTLY)
         elif "gemma-3n" in lowered_model_name:
