7dc7e0ee44000f28351f12bb5699d9ea660adb47
Daniel Han <danielhanchen@gmail.com>
2025-07-23 06:08:31 -0700

    Update _utils.py

--- unsloth/models/_utils.py
+++ unsloth/models/_utils.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "2025.7.8"
+__version__ = "2025.7.9"
 
 __all__ = [
     "SUPPORTS_BFLOAT16",
