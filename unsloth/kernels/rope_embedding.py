39c060a64ad25131c1171ae9defe8ef9d6ec1882
Datta Nimmaturi <venkatadattasainimmaturi@gmail.com>
2025-07-27 16:50:47 +0530

    Return new tensors and no sync

--- unsloth/kernels/rope_embedding.py
+++ unsloth/kernels/rope_embedding.py
@@ -156,9 +156,7 @@ pass
 def fast_rope_embedding(Q, K, cos, sin):
     Q = Fast_RoPE_Embedding.apply(Q.transpose(1, 2), cos, sin).transpose(1, 2)
     K = Fast_RoPE_Embedding.apply(K.transpose(1, 2), cos, sin).transpose(1, 2)
-    for i in range(DEVICE_COUNT):
-        torch.cuda.synchronize(f'cuda:{i}')
-    pass
+    torch.cuda.current_stream(Q.device).synchronize()
     return Q, K
 pass
 
