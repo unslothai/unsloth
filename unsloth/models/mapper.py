f8063a65946b701a9cb23f57c4a7e22d0ac29cf4
shashikanth <a.shashikanth@gmail.com>
2025-08-14 13:37:12 +0530

    merge fixes

--- unsloth/models/mapper.py
+++ unsloth/models/mapper.py
@@ -913,6 +913,38 @@ if not has_mps:
             "mistralai/Devstral-Small-2507",
             "unsloth/Devstral-Small-2507-bnb-4bit",
         ),
+        "unsloth/Qwen3-30B-A3B-Thinking-2507" : (
+            "unsloth/Qwen3-30B-A3B-Thinking-2507",
+            "Qwen/Qwen3-30B-A3B-Thinking-2507",
+        ),
+        "unsloth/Qwen3-30B-A3B-Instruct-2507" : (
+            "unsloth/Qwen3-30B-A3B-Instruct-2507",
+            "Qwen/Qwen3-30B-A3B-Instruct-2507",
+        ),
+        "unsloth/Qwen3-Coder-30B-A3B-Instruct" : (
+            "unsloth/Qwen3-Coder-30B-A3B-Instruct",
+            "Qwen/Qwen3-Coder-30B-A3B-Instruct",
+        ),
+        "unsloth/gpt-oss-20b-unsloth-bnb-4bit" : (
+            "unsloth/gpt-oss-20b",
+            "openai/gpt-oss-20b",
+            "unsloth/gpt-oss-20b-unsloth-bnb-4bit",
+        ),
+        "unsloth/gpt-oss-120b-unsloth-bnb-4bit" : (
+            "unsloth/gpt-oss-120b",
+            "openai/gpt-oss-120b",
+            "unsloth/gpt-oss-120b-unsloth-bnb-4bit",
+        ),
+        "unsloth/Qwen3-4B-Instruct-2507-unsloth-bnb-4bit" : (
+            "unsloth/Qwen3-4B-Instruct-2507",
+            "Qwen/Qwen3-4B-Instruct-2507",
+            "unsloth/Qwen3-4B-Instruct-2507-bnb-4bit",
+        ),
+        "unsloth/Qwen3-4B-Thinking-2507-unsloth-bnb-4bit" : (
+            "unsloth/Qwen3-4B-Thinking-2507",
+            "Qwen/Qwen3-4B-Thinking-2507",
+            "unsloth/Qwen3-4B-Thinking-2507-bnb-4bit",
+        ),
     }
 else:
     __INT_TO_FLOAT_MAPPER = \
@@ -1007,38 +1039,6 @@ else:
     "shashikanth-a/Llama-3.2-3B-Instruct-4bit" : (
         "unsloth/Llama-3.2-3B-Instruct",
         "meta-llama/Llama-3.2-3B-Instruct",
-    ),
-    "unsloth/Qwen3-30B-A3B-Thinking-2507" : (
-        "unsloth/Qwen3-30B-A3B-Thinking-2507",
-        "Qwen/Qwen3-30B-A3B-Thinking-2507",
-    ),
-    "unsloth/Qwen3-30B-A3B-Instruct-2507" : (
-        "unsloth/Qwen3-30B-A3B-Instruct-2507",
-        "Qwen/Qwen3-30B-A3B-Instruct-2507",
-    ),
-    "unsloth/Qwen3-Coder-30B-A3B-Instruct" : (
-        "unsloth/Qwen3-Coder-30B-A3B-Instruct",
-        "Qwen/Qwen3-Coder-30B-A3B-Instruct",
-    ),
-    "unsloth/gpt-oss-20b-unsloth-bnb-4bit" : (
-        "unsloth/gpt-oss-20b",
-        "openai/gpt-oss-20b",
-        "unsloth/gpt-oss-20b-unsloth-bnb-4bit",
-    ),
-    "unsloth/gpt-oss-120b-unsloth-bnb-4bit" : (
-        "unsloth/gpt-oss-120b",
-        "openai/gpt-oss-120b",
-        "unsloth/gpt-oss-120b-unsloth-bnb-4bit",
-    ),
-    "unsloth/Qwen3-4B-Instruct-2507-unsloth-bnb-4bit" : (
-        "unsloth/Qwen3-4B-Instruct-2507",
-        "Qwen/Qwen3-4B-Instruct-2507",
-        "unsloth/Qwen3-4B-Instruct-2507-bnb-4bit",
-    ),
-    "unsloth/Qwen3-4B-Thinking-2507-unsloth-bnb-4bit" : (
-        "unsloth/Qwen3-4B-Thinking-2507",
-        "Qwen/Qwen3-4B-Thinking-2507",
-        "unsloth/Qwen3-4B-Thinking-2507-bnb-4bit",
     ),
 }
 
