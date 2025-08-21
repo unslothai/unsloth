3f982620a575c0117aafc572c4767d77ced7304b
Daniel Han <danielhanchen@gmail.com>
2025-08-21 05:47:58 -0700

    Update loader.py

--- unsloth/models/loader.py
+++ unsloth/models/loader.py
@@ -636,7 +636,7 @@ class FastModel(FastBaseModel):
                 # Set down projection compute dtype to be float32 for float16 machines
                 # Set norms to float32 since anyways they get upcasted to float32
                 os.environ["UNSLOTH_FORCE_CUSTOM_DTYPE"] = \
-                    "torch.float16;torch.bfloat16;torch.float16;"\
+                    "torch.float16;torch.bfloat16;torch.bfloat16;"\
                     "if ('_proj' in name) and hasattr(module, 'weight') and "\
                     "torch.amax(dequantize_module_weight(module)) >= 0:"\
                     "module._pre_set_compute_dtype = torch.bfloat16\n"\
