eb65ecf83b18ef9c0bf4680ad7371797a9319cfd
pluesclues <136766175+pluesclues@users.noreply.github.com>
2025-09-05 12:40:31 -0400

    Update rl.py

--- unsloth/models/rl.py
+++ unsloth/models/rl.py
@@ -33,7 +33,11 @@ from .rl_replacements import (
     RL_CONFIG_CHANGES,
     RL_METRICS_CHANGES,
 )
+
 selective_log_softmax = RL_REPLACEMENTS["selective_log_softmax"]
+create_completion_attention_mask = RL_REPLACEMENTS["create_completion_attention_mask"] 
+calculate_pad_tokens_in_prompt = RL_REPLACEMENTS["calculate_pad_tokens_in_prompt"] 
+left_pack_padding = RL_REPLACEMENTS["left_pack_padding"]
 
 torch_compile_options = {
     "epilogue_fusion"   : True,
@@ -83,89 +87,6 @@ def PatchRL(FastLanguageModel):
             pass
         pass
     pass
-    
-    from transformers import Trainer
-    from transformers.trainer_pt_utils import nested_detach
-    @torch.no_grad()    
-    def unsloth_prediction_step(self, model, inputs, prediction_loss_only,ignore_keys,):
-        """
-        Perform an evaluation step on `model` using `inputs`.
-
-        Subclass and override to inject custom behavior.
-
-        Args:
-            model (`nn.Module`):
-                The model to evaluate.
-            inputs (`Dict[str, Union[torch.Tensor, Any]]`):
-                The inputs and targets of the model.
-
-                The dictionary will be unpacked before being fed to the model. Most models expect the targets under the
-                argument `labels`. Check your model's documentation for all accepted arguments.
-            prediction_loss_only (`bool`):
-                Whether or not to return the loss only.
-            ignore_keys (`List[str]`, *optional*):
-                A list of keys in the output of your model (if it is a dictionary) that should be ignored when
-                gathering predictions.
-
-        Return:
-            Tuple[Optional[torch.Tensor], Optional[torch.Tensor], Optional[torch.Tensor]]: A tuple with the loss,
-            logits and labels (each being optional).
-        """
-        has_labels = False if len(self.label_names) == 0 else all(inputs.get(k) is not None for k in self.label_names)
-        # For CLIP-like models capable of returning loss values.
-        # If `return_loss` is not specified or being `None` in `inputs`, we check if the default value of `return_loss`
-        # is `True` in `model.forward`.
-        return_loss = inputs.get("return_loss", None)
-        if return_loss is None:
-            return_loss = self.can_return_loss
-        loss_without_labels = True if len(self.label_names) == 0 and return_loss else False
-
-        inputs = self._prepare_inputs(inputs)
-        if ignore_keys is None:
-            if hasattr(self.model, "config"):
-                ignore_keys = getattr(self.model.config, "keys_to_ignore_at_inference", [])
-            else:
-                ignore_keys = []
-
-        # labels may be popped when computing the loss (label smoothing for instance) so we grab them first.
-        if has_labels or loss_without_labels:
-            labels = nested_detach(tuple(inputs.get(name) for name in self.label_names))
-            if len(labels) == 1:
-                labels = labels[0]
-        else:
-            labels = None
-
-        with torch.no_grad():
-            if has_labels or loss_without_labels:
-                with self.compute_loss_context_manager():
-                    loss, outputs = self.compute_loss(model, inputs, return_outputs=True)
-                loss = loss.mean().detach()
-
-                if isinstance(outputs, dict):
-                    logits = tuple(v for k, v in outputs.items() if k not in ignore_keys + ["loss"])
-                else:
-                    logits = outputs[1:]
-            else:
-                loss = None
-                with self.compute_loss_context_manager():
-                    tokenized_output = self.processing_class(inputs["prompt"], padding=True, truncation=True, return_tensors="pt").to(model.device)
-                    outputs = model(**tokenized_output)
-                if isinstance(outputs, dict):
-                    logits = tuple(v for k, v in outputs.items() if k not in ignore_keys)
-                else:
-                    logits = outputs
-                # TODO: this needs to be fixed and made cleaner later.
-                if self.args.past_index >= 0:
-                    self._past = outputs[self.args.past_index - 1]
-
-        if prediction_loss_only:
-            return (loss, None, None)
-
-        logits = nested_detach(logits)
-        if len(logits) == 1:
-            logits = logits[0]
-
-        return (loss, logits, labels)
 
     import trl.trainer
     trainers = dir(trl.trainer)
@@ -177,7 +98,6 @@ def PatchRL(FastLanguageModel):
         if hasattr(current_trainer, unwrap):
             try: exec(f"trl.trainer.{trainer}.{unwrap} = unsloth_{unwrap}")
             except: continue
-    exec(f"Trainer.prediction_step=unsloth_prediction_step")
     pass
 pass
 
@@ -202,6 +122,11 @@ torch_compile_options = {{
 }}
 
 {selective_log_softmax_code}
+
+{create_completion_attention_mask_code}
+{calculate_pad_tokens_in_prompt_code}
+{left_pack_padding_code}
+
 {RL_pre}
 
 @dataclass
@@ -779,6 +704,11 @@ def _patch_trl_rl_trainers(trainer_file = "grpo_trainer"):
     # Selective log softmax
     selective_log_softmax_code = inspect.getsource(selective_log_softmax)
 
+    #GRPO masking code
+    create_completion_attention_mask_code = inspect.getsource(create_completion_attention_mask)
+    calculate_pad_tokens_in_prompt_code = inspect.getsource(calculate_pad_tokens_in_prompt)
+    left_pack_padding_code = inspect.getsource(left_pack_padding)
+
     # Get final source code
     RLTrainer_source = RLTrainer_replacement.format(
         RLTrainer_name       = RLTrainer_name,
@@ -804,6 +734,10 @@ def _patch_trl_rl_trainers(trainer_file = "grpo_trainer"):
         max_seq_length_post  = max_seq_length_post,
 
         selective_log_softmax_code = selective_log_softmax_code,
+        create_completion_attention_mask_code = create_completion_attention_mask_code, 
+        calculate_pad_tokens_in_prompt_code = calculate_pad_tokens_in_prompt_code,
+        left_pack_padding_code = left_pack_padding_code,
+
     )
 
     if RLTrainer_name == "SFTTrainer":
@@ -868,34 +802,6 @@ def patch_functions(RLTrainer, trainer_file, RLTrainer_name, all_imports, import
     init = init.replace("if peft_config is None:", "if False:")
     init = init.replace("if peft_config is not None:", "if False:")
     init = init.replace("get_peft_model(model, peft_config)", "model")
-    if RLTrainer_name == "OnlineDPOTrainer" or RLTrainer_name == "RewardTrainer":
-        pattern = re.compile(
-            r'(^(\s*)# Add tags for models that have been loaded with the correct transformers version)',
-            re.MULTILINE
-            )
-        if RLTrainer_name == "OnlineDPOTrainer":
-            replacement = (
-                r'\2# Check for nested model attribute before setting right padding\n'
-                r'\2if hasattr(self.model.model, "model"):\n'
-                r'\2    self.model.model.model._needs_right_padding = True\n'
-                r'\2else:\n'
-                r'\2    self.model.model._needs_right_padding = True\n'
-                r'\2# Check for nested model attribute before setting right padding\n'
-                r'\2if hasattr(self.reward_model, "model"):\n'
-                r'\2    self.reward_model.model._needs_right_padding = True\n'
-                r'\2print("Unsloth reward model type: ", type(self.reward_model.model))'
-                r'\1'
-            )
-        else:
-            replacement = (
-                    r'\2# Check for nested model attribute before setting right padding\n'
-                    r'\2if hasattr(self.model.model, "model"):\n'
-                    r'\2    self.model.model.model._needs_right_padding = True\n'
-                    r'\2else:\n'
-                    r'\2    self.model.model._needs_right_padding = True\n'
-                    r'\1'
-                )
-        init = pattern.sub(replacement, init, count=1)
     # New TRL 0.20.0
     init = init.replace("if peft_config is not None or (is_peft_available() and isinstance(model, PeftModel)):", "if False:")
     # New TRL 0.20.0
