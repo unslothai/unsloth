450b2da52f45722b62c4084699f5102869577193
Daniel Han <danielhanchen@gmail.com>
2025-09-20 22:23:10 -0700

    Update vision.py

--- unsloth/models/vision.py
+++ unsloth/models/vision.py
@@ -244,7 +244,7 @@ def unsloth_base_fast_generate(
         if cache_implementation is not None:
             kwargs["compile_config"] = _compile_config
     pass
-    kwargs["cache_implementation"] = None
+    # kwargs["cache_implementation"] = None
     print(cache_implementation)
     with torch.inference_mode(), autocaster:
         output = self._old_generate(*args, **kwargs)
