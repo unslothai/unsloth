e20010c119446c7af76ac905211945114e979427
Roland Tannous <rolandtannous@gravityq.ai>
2025-10-09 08:13:54 +0000

    add gptoss BF16 to models mapper

--- unsloth/models/mapper.py
+++ unsloth/models/mapper.py
@@ -934,6 +934,7 @@ __INT_TO_FLOAT_MAPPER = \
     ),
     "unsloth/gpt-oss-120b-unsloth-bnb-4bit" : (
         "unsloth/gpt-oss-120b",
+        "unsloth/gpt-oss-120b-BF16",
         "openai/gpt-oss-120b",
         "unsloth/gpt-oss-120b-unsloth-bnb-4bit",
     ),
