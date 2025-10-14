74fec99a48362ba4c76f4da93e374683b0df9721
Daniel Han <danielhanchen@gmail.com>
2025-10-14 07:20:22 -0700

    Update mapper.py

--- unsloth/models/mapper.py
+++ unsloth/models/mapper.py
@@ -1012,6 +1012,26 @@ __INT_TO_FLOAT_MAPPER = \
         "unsloth/granite-4.0-h-small-base",
         "ibm-granite/granite-4.0-h-small-base",
     ),
+    "unsloth/Qwen3-VL-4B-Thinking-unsloth-bnb-4bit" : (
+        "unsloth/Qwen3-VL-4B-Thinking",
+        "Qwen/Qwen3-VL-4B-Thinking",
+        "unsloth/Qwen3-VL-4B-Thinking-bnb-4bit",
+    ),
+    "unsloth/Qwen3-VL-8B-Thinking-unsloth-bnb-4bit" : (
+        "unsloth/Qwen3-VL-8B-Thinking",
+        "Qwen/Qwen3-VL-8B-Thinking",
+        "unsloth/Qwen3-VL-8B-Thinking-bnb-4bit",
+    ),
+    "unsloth/Qwen3-VL-4B-Instruct-unsloth-bnb-4bit" : (
+        "unsloth/Qwen3-VL-4B-Instruct",
+        "Qwen/Qwen3-VL-4B-Instruct",
+        "unsloth/Qwen3-VL-4B-Instruct-bnb-4bit",
+    ),
+    "unsloth/Qwen3-VL-8B-Instruct-unsloth-bnb-4bit" : (
+        "unsloth/Qwen3-VL-8B-Instruct",
+        "Qwen/Qwen3-VL-8B-Instruct",
+        "unsloth/Qwen3-VL-8B-Instruct-bnb-4bit",
+    ),
 }
 
 INT_TO_FLOAT_MAPPER  = {}
