b23c8ede624d4acb48b96f246d31d731e6318593
Daniel Han <danielhanchen@gmail.com>
2025-10-26 22:39:38 -0700

    Fixes

--- unsloth/models/mistral.py
+++ unsloth/models/mistral.py
@@ -298,7 +298,9 @@ def MistralForCausalLM_fast_forward(
     else:
         RETURN_LOGITS = os.environ.get("UNSLOTH_RETURN_LOGITS", "0") == "1"
         # < 1024 Normal Unsloth uses less VRAM!
-        if bsz * q_len <= 1024: RETURN_LOGITS = True
+        if bsz * q_len <= 1024 and not RETURN_LOGITS:
+            # Use unsloth_fused_ce_loss which actually calculates the best chunk size to reduce VRAM usage
+            RETURN_LOGITS = False
 
         if not RETURN_LOGITS and labels is not None:
             n_items = kwargs.get("num_items_in_batch", None) or kwargs.get("n_items", None)
