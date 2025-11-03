6cd3f52099fda2b634f0889ce574a743c2deca0a
Daniel Han <danielhanchen@gmail.com>
2025-11-03 06:42:37 -0800

    Versioning

--- unsloth/__init__.py
+++ unsloth/__init__.py
@@ -61,7 +61,7 @@ from importlib.metadata import PackageNotFoundError
 # Check for unsloth_zoo
 try:
     unsloth_zoo_version = importlib_version("unsloth_zoo")
-    if Version(unsloth_zoo_version) < Version("2025.10.13"):
+    if Version(unsloth_zoo_version) < Version("2025.11.1"):
         print(
             "Unsloth: Please update Unsloth and Unsloth-Zoo to the latest version!\n"\
             "Do this via `pip install --upgrade --force-reinstall --no-cache-dir --no-deps unsloth unsloth_zoo`"
