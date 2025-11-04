700dd55f820a79fb4fee1aa9ad916cb694558b10
Datta Nimmaturi <venkatadattasainimmaturi@gmail.com>
2025-11-04 07:42:06 +0000

    spacessss

--- unsloth/models/_utils.py
+++ unsloth/models/_utils.py
@@ -1852,7 +1852,7 @@ def verify_fp8_support_if_applicable(model_config):
     if quant_method == 'fbgemm_fp8' and major_version < 9:
         # While L4 does support FP8 as data type, it doesn't have fbgemm (package) support yet. So we restrict it.
         raise ValueError(f"Unsloth: FBGEMM FP8 quantization is only supported on H100 and higher GPUs. L4 is not supported. You are using {torch.cuda.get_device_name()}. Refer to https://developer.nvidia.com/cuda-gpus for more details.")
-    if quant_method == 'fp8' and major_version*10 + minor_version < 89:
+    if quant_method == 'fp8' and major_version * 10 + minor_version < 89:
         # In case of block quantized, we allow L4 because we fall back to torchao kernels.
         raise ValueError(f"Unsloth: FP8 quantization is only supported on L4 and higher GPUs with compute capability 8.9 or higher. You are using {torch.cuda.get_device_name()}. Refer to https://developer.nvidia.com/cuda-gpus for more details.")
 pass
