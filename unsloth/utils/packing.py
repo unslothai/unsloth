aa063de198f44822b2a7e7d0d9b97a4bc5e705c7
Dan Saunders <danjsaund@gmail.com>
2025-12-10 10:49:52 -0500

    update TRL filter (#3707)
    * update TRL filter
    
    * both filters
    
    * Apply suggestion from @danielhanchen
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: Daniel Han <danielhanchen@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- unsloth/utils/packing.py
+++ unsloth/utils/packing.py
@@ -68,9 +68,14 @@ def _get_cached_block_mask(
 
 
 class _TrlPackingWarningFilter(logging.Filter):
+    to_filter = (
+        "attention implementation is not",
+        "kernels-community",
+    )
+
     def filter(self, record: logging.LogRecord) -> bool:
         message = record.getMessage()
-        return not "kernels-community" in message
+        return not any(substring in message for substring in self.to_filter)
 
 
 _TRL_FILTER_INSTALLED = False
