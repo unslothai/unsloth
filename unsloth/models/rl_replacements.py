119af42947bca834b51afec80c7922860802c8bb
pluesclues <136766175+pluesclues@users.noreply.github.com>
2025-12-19 17:19:40 -0500

    Adjust chunk size for logprobs calculation

--- unsloth/models/rl_replacements.py
+++ unsloth/models/rl_replacements.py
@@ -736,7 +736,7 @@ def grpo_trainer__get_per_token_logps_and_entropies(function_name, function):
                             logits_chunk,
                             lm_head,
                             completion_input_ids_chunk,
-                            chunks = 8,
+                            chunks = input_ids.shape[0]*2,
                             logit_scale_multiply = logit_scale_multiply,
                             logit_scale_divide = logit_scale_divide,
                             logit_softcapping = logit_softcapping,
