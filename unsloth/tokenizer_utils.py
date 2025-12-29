5bbdc0a34b0f2f84fc78d315a238ac528b358f46
Datta Nimmaturi <venkatadattasainimmaturi@gmail.com>
2025-12-29 04:21:42 +0000

    fix for qwen3-guard tokenizer

--- unsloth/tokenizer_utils.py
+++ unsloth/tokenizer_utils.py
@@ -601,7 +601,10 @@ def load_correct_tokenizer(
     old_chat_template = getattr(tokenizer, "chat_template", None)
 
     # Ignore mistral type models since they don't have an add_generation_prompt
-    if "mistral" in str(getattr(tokenizer, "name_or_path", "")).lower():
+    if (
+        "mistral" in str(getattr(tokenizer, "name_or_path", "")).lower()
+        or "guard" in str(getattr(tokenizer, "name_or_path", "")).lower()
+    ):
         chat_template = old_chat_template
 
     # Also check Llama-2 old style models
