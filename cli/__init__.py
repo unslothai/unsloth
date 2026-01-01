f47ebfd237ee183390313edbe1325388866dec72
Dan Saunders <danjsaund@gmail.com>
2026-01-01 13:50:22 -0500

    CLI command for UI

--- cli/__init__.py
+++ cli/__init__.py
@@ -3,6 +3,7 @@ import typer
 from cli.commands.train import train
 from cli.commands.inference import inference
 from cli.commands.export import export, list_checkpoints
+from cli.commands.ui import ui
 
 app = typer.Typer(
     help="Command-line interface for Unsloth training, inference, and export.",
@@ -13,3 +14,4 @@ app.command()(train)
 app.command()(inference)
 app.command()(export)
 app.command("list-checkpoints")(list_checkpoints)
+app.command()(ui)
