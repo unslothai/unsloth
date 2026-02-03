c17ba10f96d5bbee99d8787fdd6b27350f549954
Roland Tannous <rolandtannous@gravityq.ai>
2026-02-03 16:57:57 +0000

    refactor/inference-api-routes-part-1

--- studio/backend/routes/__init__.py
+++ studio/backend/routes/__init__.py
@@ -4,5 +4,6 @@ API Routes
 
 from routes.training import router as training_router
 from routes.models import router as models_router
+from routes.inference import router as inference_router
 
-__all__ = ["training_router", "models_router"]
+__all__ = ["training_router", "models_router", "inference_router"]
