a9468146f2bad4baefc45b813b49f66df1f58e9b
Wilbatronic <65288629+Wilbatronic@users.noreply.github.com>
2026-02-07 11:25:58 +0000

    Update loader.py

--- unsloth/models/loader.py
+++ unsloth/models/loader.py
@@ -39,7 +39,7 @@ from .loader_utils import (
     _tag_model_with_fp8_torchao_config,
     get_model_name,
 )
-import os, contextlib, sys
+import os, contextlib, sys, warnings
 
 try:
     from huggingface_hub import get_token
