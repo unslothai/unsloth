dcb5f04cbc438f5cae79665db03a69394687c8c4
Daniel Han <danielhanchen@gmail.com>
2026-03-27 08:13:58 +0000

    chore: temporarily remove test files (will be added in a follow-up)

--- tests/python/conftest.py
+++
@@ -1,7 +0,0 @@
-"""Shared pytest configuration for tests/python/."""
-
-
-def pytest_configure(config):
-    config.addinivalue_line(
-        "markers", "server: heavyweight tests requiring studio venv"
-    )
