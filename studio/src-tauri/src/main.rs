726abd5e6b1888dd5bfb1c47b0b1f9e94b51d5f9
Wasim Yousef Said <wasimysdev@gmail.com>
2026-05-05 09:09:48 +0200

    Add Tauri native notifications (#5273)

--- studio/src-tauri/src/main.rs
+++ studio/src-tauri/src/main.rs
@@ -167,6 +167,7 @@ fn main() {
         }))
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_clipboard_manager::init())
