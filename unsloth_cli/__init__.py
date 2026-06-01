8a3c7866ac56256de762f70093549f19b35db3cf
Dariton4000 <184837391+Dariton4000@users.noreply.github.com>
2026-06-01 20:54:42 +0200

    -Added catch for unsloth running inside of system32 (for windows) - changed import statement to import os as _os instead of only os.path as _osp - replaced __osp mention with __os.path

--- unsloth_cli/__init__.py
+++ unsloth_cli/__init__.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # Copyright 2026-present the Unsloth AI Inc. team. All rights reserved. See /studio/LICENSE.AGPL-3.0
 
-import os.path as _osp
+import os as _os
 import sys as _sys
 
 import typer
@@ -20,7 +20,7 @@ from unsloth_cli.commands.studio import (
 
 # Canonicalise `-np<N>` only under the `unsloth` console-script;
 # third-party scripts that import unsloth_cli keep their argv intact.
-_entry_base = _osp.basename(_sys.argv[0]).lower() if _sys.argv else ""
+_entry_base = _os.path.basename(_sys.argv[0]).lower() if _sys.argv else ""
 if _entry_base in {"unsloth", "unsloth.exe"}:
     _expand_attached_np_short()
 del _entry_base
@@ -53,8 +53,18 @@ def main(
         help = "Show version and exit.",
     ),
 ):
-    pass
-
+    if _sys.platform == "win32": # this block catches unsloth running inside of System32 or any subdirs, this WILL cause errors if not prevented.
+        _cwd = _os.path.normcase(_os.path.normpath(_os.getcwd()))
+        _system32 = _os.path.normcase(_os.path.join(_os.environ.get("WINDIR", r"C:\Windows"), "System32"))
+        if _cwd == _system32 or _cwd.startswith(_system32 + _os.sep):
+            typer.secho(
+                "Refusing to run Unsloth inside System32 as it will lead to Errors.\n"
+                "cd to a normal working directory and try again.",
+                fg = typer.colors.RED,
+                err = True,
+            )
+            raise typer.Exit(code = 1)
+        
 
 app.command()(train)
 app.command()(inference)
