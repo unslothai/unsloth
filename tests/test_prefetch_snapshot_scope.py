36164a079eb80011e5afe3f2a18ae9c26f622e78
danielhanchen <danielhanchen@gmail.com>
2026-07-05 23:30:00 +0000

    Add the AGPL license header to the new prefetch-scope test
    Give tests/test_prefetch_snapshot_scope.py the standard AGPL-3.0 license header
    so the new file carries a license notice.

--- tests/test_prefetch_snapshot_scope.py
+++ tests/test_prefetch_snapshot_scope.py
@@ -1,3 +1,19 @@
+# Unsloth Zoo - Utilities for Unsloth
+# Copyright 2023-present Daniel Han-Chen, Michael Han-Chen & the Unsloth team. All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 """Pure-CPU, no-network unit tests for prefetch snapshot scoping in unsloth/models/_utils.py.
 
 maybe_prefetch_hf_snapshot warms the HF cache before the in-process load. The warm must cover at
