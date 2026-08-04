15d33a1761ddc8127113a31a8c691a17f389d620
danielhanchen <unslothshared@gmail.com>
2026-08-04 20:09:36 +0000

    Use this repository's Apache-2.0 header on the new test files
    unsloth is Apache-2.0 (LICENSE, and pyproject license = "Apache-2.0"), and 303
    of its 314 test files carry no header at all while 10 carry the Apache one. The
    new files here went in with an AGPL-3.0 notice attributed to Unsloth Zoo, which
    is the wrong licence, the wrong project and against the convention here.

--- tests/test_offloaded_parameter_hint.py
+++ tests/test_offloaded_parameter_hint.py
@@ -1,18 +1,16 @@
-# Unsloth Zoo - Utilities for Unsloth
-# Copyright 2023-present Daniel Han-Chen, Michael Han-Chen & the Unsloth team. All rights reserved.
+# Copyright 2023-present Daniel Han-Chen & the Unsloth team. All rights reserved.
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 """A save that fails because the model was offloaded should say so.
 
