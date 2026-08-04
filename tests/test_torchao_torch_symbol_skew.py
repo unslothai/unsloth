341ce2145ee8e53e2cf906241053803833952d85
danielhanchen <unslothshared@gmail.com>
2026-08-04 19:36:38 +0000

    Add the AGPL-3.0 header to the new test files

--- tests/test_torchao_torch_symbol_skew.py
+++ tests/test_torchao_torch_symbol_skew.py
@@ -1,3 +1,19 @@
+# Unsloth Zoo - Utilities for Unsloth
+# Copyright 2023-present Daniel Han-Chen, Michael Han-Chen & the Unsloth team. All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 """torchao 0.18 must not be able to kill `import unsloth`.
 
 torchao 0.17 guarded the import:
