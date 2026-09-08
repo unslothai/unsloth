292b1680733aab3f075b97324fdbd8e064817f77
Rudra Bhaskar <rbrudra9439@gmail.com>
2026-09-08 23:20:52 +0530

    Drop unrelated formatting from the torchcodec PR

--- unsloth_cli/commands/studio.py
+++ unsloth_cli/commands/studio.py
@@ -1201,6 +1201,7 @@ def _bind_is_wildcard(host: str) -> bool:
     is not a new cost here.
     """
     from unsloth_cli._tool_policy import is_wildcard_host
+
     return is_wildcard_host(host)
 
 
