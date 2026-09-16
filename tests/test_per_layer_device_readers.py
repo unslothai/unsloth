8f0efc50d0822eee274863cee83b8de5d0d52d3c
danielhanchen <danielhanchen@gmail.com>
2026-09-16 08:03:07 +0000

    Declare the AGPL-3.0 SPDX identifier on the new test file

--- tests/test_per_layer_device_readers.py
+++ tests/test_per_layer_device_readers.py
@@ -1,18 +1,5 @@
-# Unsloth - Fast finetuning of LLMs
-# Copyright 2023-present Daniel Han-Chen, Michael Han-Chen & the Unsloth team. All rights reserved.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# SPDX-License-Identifier: AGPL-3.0-only
+# Copyright 2026-present the Unsloth AI Inc. team. All rights reserved.
 
 """The pipeline-parallel inference readers must survive a layer with no device index.
 
