f54ecf58b844287d2d8370162dc0b052edb685a5
danielhanchen <danielhanchen@gmail.com>
2026-09-22 15:21:38 +0000

    Add the SPDX line the other test files carry

--- tests/test_compressed_tensors_bnb.py
+++ tests/test_compressed_tensors_bnb.py
@@ -1,3 +1,4 @@
+# SPDX-License-Identifier: AGPL-3.0-only
 # Copyright 2023-present Daniel Han-Chen & the Unsloth team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
