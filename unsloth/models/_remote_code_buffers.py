35dc732f6f215b68127ed44cd9d491b1c158cfbe
Daniel Han <danielhanchen@gmail.com>
2026-09-23 07:45:26 +0000

    Use the AGPL-3.0 header on the new remote-code buffer module

--- unsloth/models/_remote_code_buffers.py
+++ unsloth/models/_remote_code_buffers.py
@@ -1,13 +1,15 @@
+# SPDX-License-Identifier: AGPL-3.0-only
 # Copyright 2023-present Daniel Han-Chen & the Unsloth team. All rights reserved.
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#     http://www.apache.org/licenses/LICENSE-2.0
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 """transformers 5 gives non-persistent buffers `torch.empty_like` storage and leaves them to
 `_init_weights`; 4.x-era remote code computes them in `__init__` (RoPE inv_freq, decay slopes),
